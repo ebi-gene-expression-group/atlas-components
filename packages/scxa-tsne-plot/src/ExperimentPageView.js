afe6a351d61097eccc677ed6ef5f68a0fbace8fb
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-09-22 19:51:06 +0100

    Abstract this component from React Router; provide all Highcharts plotOptions and add more presentation-related props Remove all references to params, queryString and URI. The previous merge of plotOptions overwrote the field, so we need to provide it in its entirety. Change endpoint for gene expression plot to display clusters. Add props to make colours configurable.

--- packages/scxa-tsne-plot/src/ExperimentPageView.js
+++ packages/scxa-tsne-plot/src/ExperimentPageView.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import URI from 'urijs'
-import queryString from 'query-string'
-
 import {_colourizeClusters, _colourizeExpressionLevel} from './SeriesMapper'
 import TSnePlot from 'expression-atlas-tsne-plot'
 import AtlasAutocomplete from 'atlas-autocomplete'
@@ -12,33 +9,20 @@ class ExperimentPageView extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      params: queryString.parse(props.location.search),
-      geneId: ``,
-      k: 0,
-      clusterId: [],
+      geneId: this.props.geneId,
+      k: this.props.k || this.props.availableClusters[0],
+      highlightClusters: this.props.highlightClusters
     }
+
+    this._handleChange = this._handleChange.bind(this)
   }
 
-  handleChange(param, item) {
-    let _newparam = {}
-    _newparam[param] = (param === `k` ? Number.parseInt(item.target.value) : item)
-    this.setState(_newparam)
-
-    this.props.history.push(
-      `?` +
-      queryString.stringify({
-        geneId: (param === `geneId` ? item : this.state.geneId),
-        k: (param === `k` ? item.target.value : this.state.k)})
-      + `&clusterId=[${this.state.clusterId}]`
-    )
-  }
-
-  componentDidMount() {
-    this.setState({
-      geneId: this.state.params.geneId,
-      k: this.state.params.k !== undefined ? Number.parseInt(this.state.params.k) : this.state.k,
-      clusterId: JSON.parse(this.state.params.clusterId || `[]`)
-    })
+  _handleChange(stateField) {
+    return (event) => {
+      this.setState({
+        [stateField]: event.target ? event.target.value : event
+      })
+    }
   }
 
   render() {
@@ -47,10 +31,15 @@ class ExperimentPageView extends React.Component {
         <option key={ix} value={name}>{name}</option>
       ))
 
-    const selectedCluster = this.state.k ? this.state.k : this.props.availableClusters[0]
-
     const clusterHighchartsConfig = {
       plotOptions: {
+        series: {
+          turboThreshold: 0,
+          animation: false,
+          marker: {
+            radius: 4
+          }
+        },
         scatter: {
           tooltip: {
             headerFormat: `<b>Cluster {series.name}</b><br>`,
@@ -62,6 +51,13 @@ class ExperimentPageView extends React.Component {
 
     const geneExpressionHighchartsConfig = {
       plotOptions: {
+        series: {
+          turboThreshold: 0,
+          animation: false,
+          marker: {
+            radius: 4
+          }
+        },
         scatter: {
           tooltip: {
             headerFormat: `<b>{point.key} – Cluster {series.name}</b><br>`,
@@ -75,8 +71,8 @@ class ExperimentPageView extends React.Component {
       <div className={`row`}>
 
         <div className={`small-6 columns`}>
-          <label>Clustering: {this.state.k}</label>
-          <select value={selectedCluster} onChange={this.handleChange.bind(this, `k`)}>
+          <label>Number of clusters, <i>k</i></label>
+          <select value={this.state.k} onChange={this._handleChange(`k`)}>
             {clusterOptions}
           </select>
           <TSnePlot atlasUrl={this.props.atlasUrl}
@@ -91,9 +87,9 @@ class ExperimentPageView extends React.Component {
                              suggesterEndpoint={this.props.suggesterEndpoint}
                              enableSpeciesFilter={false}
                              initialValue={this.state.geneId}
-                             onSelect={this.handleChange.bind(this, `geneId`)}/>
+                             onSelect={this._handleChange(`geneId`)}/>
           <TSnePlot atlasUrl={this.props.atlasUrl}
-                    sourceUrl={`json/experiments/${this.props.experimentAccession}/tsneplot/expression/${this.state.geneId}`}
+                    sourceUrl={`json/experiments/${this.props.experimentAccession}/tsneplot/clusters/${this.state.k}/expression/${this.state.geneId}`}
                     seriesMapper={_colourizeExpressionLevel(this.props.expressionColourHue)}
                     resourcesUrl={this.props.resourcesUrl}
                     highchartsConfig={geneExpressionHighchartsConfig}/>
@@ -105,13 +101,23 @@ class ExperimentPageView extends React.Component {
 }
 
 ExperimentPageView.propTypes = {
-  props: PropTypes.object,
-  atlasUrl: PropTypes.string,
+  atlasUrl: PropTypes.string.isRequired,
   resourcesUrl: PropTypes.string,
-  suggesterEndpoint: PropTypes.string,
+  suggesterEndpoint: PropTypes.string.isRequired,
+  experimentAccession: PropTypes.string.isRequired,
   availableClusters: PropTypes.array.isRequired,
+  k: PropTypes.number,
   highlightClusters: PropTypes.array,
-  expressionColourHue: PropTypes.number
+  geneId: PropTypes.string,
+  expressionColourHue: PropTypes.number,
+  expressionColourLightness: PropTypes.number
+}
+
+ExperimentPageView.defaultProps = {
+  highlightClusters: [],
+  geneId: `blah`,
+  expressionColourHue: 240,
+  expressionColourLightness: 60
 }
 
 export default ExperimentPageView
