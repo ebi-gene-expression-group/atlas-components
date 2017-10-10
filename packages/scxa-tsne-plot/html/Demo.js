d36aff03f823393f3f473f9d64510db0db3916cc
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-10 17:54:56 +0100

    Demo component allows setting the highlighted clusters

--- packages/scxa-tsne-plot/html/Demo.js
+++ packages/scxa-tsne-plot/html/Demo.js
@@ -5,14 +5,48 @@ import ExperimentPageView from '../src/index'
 class Demo extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      inputHighlightClusters: ``,
+      highlightClusters: []
+    }
+
+    this._handleChange = this._handleChange.bind(this)
+    this._handleSubmit = this._handleSubmit.bind(this)
+  }
+
+  _handleChange(event) {
+    this.setState({
+      inputHighlightClusters: event.target.value
+    })
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault()
+
+    this.setState({
+      highlightClusters: this.state.inputHighlightClusters.split(`,`).map((e) => e.trim()).filter((e) => e.length)
+    })
   }
 
   render() {
     return(
-      <ExperimentPageView atlasUrl={`https://localhost:8443/gxa_sc/`}
-                          suggesterEndpoint={`json/suggestions`}
-                          experimentAccession={`E-MTAB-4388`}
-                          availableClusters={[`2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`]}/>
+      <div className={`row column`}>
+        <div className={`row column`}>
+          <form onSubmit={this._handleSubmit}>
+            <label>Highlight clusters (cluster names separated by commas):</label>
+            <input type={`text`} onChange={this._handleChange} value={this.state.inputHighlightClusters}/>
+            <input className={`button`} type="submit" value="Submit" />
+          </form>
+        </div>
+
+        <ExperimentPageView atlasUrl={`https://localhost:8443/gxa_sc/`}
+                            suggesterEndpoint={`json/suggestions`}
+                            experimentAccession={`E-MTAB-4388`}
+                            availableClusters={[`2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`]}
+                            highlightClusters={this.state.highlightClusters}
+        />
+      </div>
     )
   }
 }
