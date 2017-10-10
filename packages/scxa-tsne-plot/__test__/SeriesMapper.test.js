85e23d92df04d724986212118d5758098c5ce21a
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-10-10 17:54:36 +0100

    Fix tests to match the new series mappers

--- packages/scxa-tsne-plot/__test__/SeriesMapper.test.js
+++ packages/scxa-tsne-plot/__test__/SeriesMapper.test.js
@@ -1,27 +1,28 @@
 import Color from 'color'
-import {_colourizeClusters, _colourizeExpressionLevel} from '../src/SeriesMapper'
-import RandomSeriesGenerator from './RandomSeriesGenerator'
-import '../src/util/MathRound10'
+import HighchartsSeriesGenerator from 'highcharts-series-generator'
+
+import {_colourizeClusters} from '../src/ClusterTSnePlot'
+import {_colourizeExpressionLevel} from '../src/GeneExpressionTSnePlot'
+import '../src/util/MathRound'
 
 const seriesNames = [`0`, `1`, `2`, `3`, `4`]
 const maxPointsPerSeries = 1000
 
 describe(`SeriesMapper.colourizeClusters`, () => {
   test(`must not change the number of series`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
     expect(_colourizeClusters([], `lightgrey`)(randomSeries)).toHaveLength(seriesNames.length)
   })
 
   test(`must not change the number of points in each series`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
     _colourizeClusters([], `lightgrey`)(randomSeries).forEach((series, i) => {
       expect(series.data).toHaveLength(randomSeries[i].data.length)
     })
   })
 
-
   test(`must not dim (i.e. add a color field) any series if all are highlighted`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
     _colourizeClusters(seriesNames, `lightgrey`)(randomSeries).forEach((series) => {
       series.data.forEach((point) => {
         expect(point).not.toHaveProperty(`color`)
@@ -30,7 +31,7 @@ describe(`SeriesMapper.colourizeClusters`, () => {
   })
 
   test(`must not dim (i.e. add a color field) any series if none are passed to highlight`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
     _colourizeClusters([], `lightgrey`)(randomSeries).forEach((series) => {
       series.data.forEach((point) => {
         expect(point).not.toHaveProperty(`color`)
@@ -39,7 +40,7 @@ describe(`SeriesMapper.colourizeClusters`, () => {
   })
 
   test(`dims all series but the highlighted ones`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
 
     // Make sure that we’re not testing the test above by highlighting at least one cluster
     let highlightRandomSeries = []
@@ -53,7 +54,7 @@ describe(`SeriesMapper.colourizeClusters`, () => {
         if (highlightRandomSeries.includes(series.name)) {
           expect(point).not.toHaveProperty(`color`)
         } else {
-          expect(point).toHaveProperty(`color`, `lightgrey`)
+          expect(point).toHaveProperty(`color`, Color(`lightgrey`).alpha(0.65).rgb().toString())
         }
       })
     })
@@ -61,37 +62,47 @@ describe(`SeriesMapper.colourizeClusters`, () => {
 })
 
 describe(`SeriesMapper.colourizeExpressionLevel`, () => {
+
+  const plotData = (series) => {
+    const allPoints = series.reduce((acc, series) => acc.concat(series.data), [])
+
+    return {
+      series: series,
+      min: Math.min(...allPoints.map((point) => point.expressionLevel)),
+      max: Math.max(...allPoints.map((point) => point.expressionLevel))
+    }
+  }
+
   const hue = 240
-  const lightness = 60
 
   test(`must not change the number of series`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
-    expect(_colourizeExpressionLevel(hue, lightness)(randomSeries)).toHaveLength(seriesNames.length)
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+    expect(_colourizeExpressionLevel(hue, [])(plotData(randomSeries))).toHaveLength(seriesNames.length)
   })
 
   test(`must not change the number of points in each series`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
-    _colourizeExpressionLevel(hue, lightness)(randomSeries).forEach((series, i) => {
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+    _colourizeExpressionLevel(hue, [])(plotData(randomSeries)).forEach((series, i) => {
       expect(series.data).toHaveLength(randomSeries[i].data.length)
     })
   })
 
-  test(`add a color field to all points`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+  test(`adds a color field to all points`, () => {
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
 
-    _colourizeExpressionLevel(hue, lightness)(randomSeries).forEach((series) => {
+    _colourizeExpressionLevel(hue, [])(plotData(randomSeries)).forEach((series) => {
       series.data.forEach((point) => {
         expect(point).toHaveProperty(`color`)
       })
     })
   })
 
-  test(`point with highest expression has the maximum colour`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+  test(`assigns maximum colour to the point with highest expression`, () => {
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
     const allPoints = randomSeries.reduce((acc, series) => acc.concat(series.data), [])
     const maxExpressionLevel = Math.round10(Math.max(...allPoints.map((point) => point.expressionLevel)), -2)
 
-    const maxExpressionLevelPoints = _colourizeExpressionLevel(hue, lightness)(randomSeries).reduce((acc, series) => {
+    const maxExpressionLevelPoints = _colourizeExpressionLevel(hue, [])(plotData(randomSeries)).reduce((acc, series) => {
       acc.push(series.data.filter((point) => point.expressionLevel === maxExpressionLevel, -2))
       return acc
     }, [])
@@ -99,16 +110,16 @@ describe(`SeriesMapper.colourizeExpressionLevel`, () => {
 
     expect(maxExpressionLevelPoints.length).toBeGreaterThanOrEqual(1)
     maxExpressionLevelPoints.forEach((point) => {
-      expect(point).toHaveProperty(`color`, Color(`hsl(${hue}, 100%, ${lightness}%)`).rgb().toString())
+      expect(point).toHaveProperty(`color`, Color(`hwb(${hue}, 0%, 0%)`).alpha(0.65).rgb().toString())
     })
   })
 
-  test(`point with lowest expression has the minimum colour`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+  test(`assigns minimum colour to the point with lowest expression`, () => {
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
     const allPoints = randomSeries.reduce((acc, series) => acc.concat(series.data), [])
     const minExpressionLevel = Math.round10(Math.min(...allPoints.map((point) => point.expressionLevel)), -2)
 
-    const minExpressionLevelPoints = _colourizeExpressionLevel(hue, lightness)(randomSeries).reduce((acc, series) => {
+    const minExpressionLevelPoints = _colourizeExpressionLevel(hue, [])(plotData(randomSeries)).reduce((acc, series) => {
       acc.push(series.data.filter((point) => point.expressionLevel === minExpressionLevel, -2))
       return acc
     }, [])
@@ -116,18 +127,59 @@ describe(`SeriesMapper.colourizeExpressionLevel`, () => {
 
     expect(minExpressionLevelPoints.length).toBeGreaterThanOrEqual(1)
     minExpressionLevelPoints.forEach((point) => {
-      expect(point).toHaveProperty(`color`, Color(`hsl(${hue}, 0%, ${lightness}%)`).rgb().toString())
+      expect(point).toHaveProperty(`color`, Color(`hwb(${hue}, 90%, 0%)`).alpha(0.65).rgb().toString())
     })
   })
 
-  test(`expression level is rounded to two decimal places`, () => {
-    const randomSeries = RandomSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
+  test(`rounds expression level to two decimal places`, () => {
+    const randomSeries = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
 
-    _colourizeExpressionLevel(hue, lightness)(randomSeries).forEach((series) => {
+    _colourizeExpressionLevel(hue, [])(plotData(randomSeries)).forEach((series) => {
       series.data.forEach((point) => {
           if (String(point.expressionLevel).includes(`.`)) {
             expect(String(point.expressionLevel).split(`.`)[1].length).toBeLessThanOrEqual(2)
           }
+        })
+    })
+  })
+
+  test(`assigns maximum colour if all points have the same expression level`, () => {
+    _colourizeExpressionLevel(hue, [])({
+      series: [
+        {
+          name: `Cluster 1`,
+          data: [
+            {
+              name: `Point 1-1`,
+              x: 1,
+              y: 1,
+              expressionLevel: 100.0
+            }
+          ]
+        },
+        {
+          name: `Cluster 2`,
+          data: [
+            {
+              name: `Point 2-1`,
+              x: 2,
+              y: 2,
+              expressionLevel: 100.0
+            },
+            {
+              name: `Point 2-2`,
+              x: 3,
+              y: 3,
+              expressionLevel: 100.0
+            }
+          ]
+        }
+      ],
+      min: 100.0,
+      max: 100.0
+    }).forEach((series) => {
+      series.data.forEach((point) => {
+          expect(point).toHaveProperty(`color`, Color(`hwb(${hue}, 0%, 0%)`).alpha(0.65).rgb().toString())
         })
     })
   })
