3f527bb1305f58788fb4e1fd421893b80799e8e7
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-05-04 10:13:42 +0100

    Container component that gets the data from FetchLoader and displays search results

---
+++ packages/scxa-faceted-search-results/src/SearchContainer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import FilterSidebar from './FilterSidebar'
+import ResultsList from './ResultsList'
+
+class SearchContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // Build initial state of selected filters from the attributes props, keeps track of what filters have been checked
+    // in the sidebar
+    this.state = {
+      selectedAttributes: []
+    }
+  }
+
+  render() {
+    <div className={`row`}>
+      <div className={`small-12 medium-4 large-2 columns`}>
+        <FilterSidebar />
+      </div>
+      <div className={`small-12 medium-8 large-10 columns`}>
+        <ResultsList />
+      </div>
+    </div>
+  }
+}
+
+SearchContainer.PropTypes = {
+  // A list of results from where facets can be inferred by inspecting each result’s attribute field
+  // Supplied by FetchLoader, or set manually for testing
+}
+
+export default SearchContainer
