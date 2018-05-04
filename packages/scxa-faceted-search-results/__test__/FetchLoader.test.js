3f2675b16cdbb1bb54d18a87a27380a129c16f24
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2018-05-04 13:11:27 +0100

    Rename test to match FetchLoader

--- packages/scxa-faceted-search-results/__test__/FetchLoader.test.js
+++ packages/scxa-faceted-search-results/__test__/FetchLoader.test.js
@@ -5,7 +5,7 @@ import {shallow, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import fetchMock from 'fetch-mock'
 
-import GeneSearchResults from '../src/GeneSearchResults'
+import FetchLoader from '../src/FetchLoader'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -16,7 +16,7 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-describe(`FacetedSearchResults`, () => {
+describe(`FetchLoader`, () => {
   beforeEach(() => {
     fetchMock.restore()
   })
@@ -30,7 +30,7 @@ describe(`FacetedSearchResults`, () => {
 
   test(`until the fetch promise is not resolved a loading message is displayed, then goes away`, async () => {
     fetchMock.get(`*`, `{}`)
-    const wrapper = mount(<GeneSearchResults {...props} />)
+    const wrapper = mount(<FetchLoader {...props} />)
 
     expect(wrapper.find(`#loader`)).toHaveLength(1)
     expect(wrapper.find(`.callout .alert`)).toHaveLength(0)
@@ -42,7 +42,7 @@ describe(`FacetedSearchResults`, () => {
 
   test(`renders an error message if request to the server returns 4xx or 5xx`, async () => {
     fetchMock.get(`*`, getRandomHttpErrorCode);
-    const wrapper = mount(<GeneSearchResults {...props} />)
+    const wrapper = mount(<FetchLoader {...props} />)
 
     await wrapper.instance().componentDidMount()
     wrapper.update()
@@ -51,7 +51,7 @@ describe(`FacetedSearchResults`, () => {
 
   test(`renders an error message if the component does not receive JSON`, async () => {
     fetchMock.get(`*`, `Break the cycle, Morty. Rise above. Focus on the science`)
-    const wrapper = mount(<GeneSearchResults {...props} />)
+    const wrapper = mount(<FetchLoader {...props} />)
 
     await wrapper.instance().componentDidMount()
     wrapper.update()
@@ -60,7 +60,7 @@ describe(`FacetedSearchResults`, () => {
 
   test(`renders an error message if the error boundary kicks in`, async () => {
     fetchMock.get(`*`, `{}`)
-    const wrapper = mount(<GeneSearchResults {...props} />)
+    const wrapper = mount(<FetchLoader {...props} />)
 
     const e = new Error(`They’re inside you building a monument to compromise!`)
     wrapper.instance().componentDidCatch(e, `Ruben’s seen some rough years, Morty.`)
