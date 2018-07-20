c9a9fab56e10d3c5b8b25bd1821d7ef282d5bd2e
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-07-20 16:09:33 +0100

    Full-fledged example for rich search feature

--- packages/scxa-faceted-search-results/html/fetchLoaderDemo.js
+++ packages/scxa-faceted-search-results/html/fetchLoaderDemo.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom'
 
 import FetchLoader from '../src/FetchLoader'
 
+import ExperimentCard from './ExperimentCard'
+
 const render = (options, target) => {
-  ReactDOM.render(<FetchLoader {...options} />, document.getElementById(target))
+  ReactDOM.render(<FetchLoader {...options}
+                               ResultElementClass={ExperimentCard}/>, document.getElementById(target))
 }
 
 export {render}
