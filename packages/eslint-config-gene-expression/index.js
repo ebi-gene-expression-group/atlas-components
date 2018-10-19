88b0a53561ee8e935190787d9c73a705a844887e
Monica Jianu <monica@ebi.ac.uk>
2018-10-19 14:26:15 +0100

    Fixed error in rules containing warning.

--- packages/eslint-config-gene-expression/index.js
+++ packages/eslint-config-gene-expression/index.js
@@ -21,8 +21,8 @@ module.exports = {
 
   rules: {
     'no-var': 'error', // Enforce using const and let, rather than var
-    semi: ['warning', 'never'], // Disallow semicolons
-    quotes: ['warning', 'backtick'], // Enforce using `` for strings
+    semi: ['warn', 'never'], // Disallow semicolons
+    quotes: ['warn', 'backtick'], // Enforce using `` for strings
     eqeqeq: ['error', 'always'], // Enforce using === and !==
     indent: ['error', 2], // Enforce indentation using 2 spaces
     'no-console': ['warning', { allow: ['warn', 'error']}] // Allow console.warn and console.error statements
