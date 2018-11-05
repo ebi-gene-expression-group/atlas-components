36d69a57275fde85d5567e9bb7b5131f4085947c
Monica Jianu <monica@ebi.ac.uk>
2018-11-05 15:35:43 +0000

    Added first version of a Highcharts heatmap that displays marker genes, with 2 Y axes showing gene IDs and the cluster where the gene is a marker.

--- packages/scxa-marker-gene-heatmap/src/FetchLoader.js
+++ packages/scxa-marker-gene-heatmap/src/FetchLoader.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import URI from 'urijs'
+import MarkerGeneHeatmap from './MarkerGeneHeatmap'
+
+const CalloutAlert = ({error}) =>
+  <div className={`row column`}>
+    <div className={`callout alert small`}>
+      <h5>Oops!</h5>
+      <p>
+        {error.description}<br/>
+        If the error persists, in order to help us debug the issue, please copy the URL and this message and
+        send it to us via <a href={`https://www.ebi.ac.uk/support/gxasc`}>the EBI Support & Feedback system</a>:
+      </p>
+      <code>{`${error.name}: ${error.message}`}</code>
+    </div>
+  </div>
+
+CalloutAlert.propTypes = {
+  error: PropTypes.shape({
+    description: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired
+  })
+}
 
 class FetchLoader extends React.Component {
   constructor(props) {
@@ -55,16 +77,19 @@ class FetchLoader extends React.Component {
     const { data, isLoading, hasError } = this.state
 
     return (
-      <div>
-        {hasError}
-      </div>
+      hasError ?
+        <CalloutAlert error={hasError}/> :
+        isLoading ?
+          <p>Loading...</p> :
+          <MarkerGeneHeatmap isLoading={isLoading} data={data} chartHeight={`800px`}/>
     )
   }
 }
 
 FetchLoader.propTypes = {
   host: PropTypes.string,
-  resource: PropTypes.string
+  resource: PropTypes.string,
+  selectedK: PropTypes.number
 }
 
 export default FetchLoader
---
+++ packages/scxa-marker-gene-heatmap/src/MarkerGeneHeatmap.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import HC_heatmap from 'highcharts/modules/heatmap'
+import _ from 'lodash'
+
+// init the module
+HC_heatmap(Highcharts)
+
+const MarkerGeneHeatmap = (props) => {
+  const {chartHeight, data} = props
+
+  const yAxisCategories = _.uniq(data.map(x => x.name))
+  const secondaryYAxisCategories = _.uniqBy(data, `name`).map(x => x.clusterIdWhereMarker)
+
+  const options = {
+    chart: {
+      type: `heatmap`,
+      height: chartHeight,
+      zoomType: `y`,
+      // plotBackgroundColor: `#D3D3D3`
+    },
+    credits: {
+      enabled: false
+    },
+    title: {
+      text: `Gene Expression Heatmap`
+    },
+    xAxis: {
+      opposite: true,
+      labels: {
+        format: `Cluster {value}`
+      },
+      endOnTick: false,
+      gridLineWidth: 0,
+      minorGridLineWidth: 0
+    },
+
+    yAxis: [{
+      categories: yAxisCategories,
+      title: {
+        text: `Gene ID`,
+        style: {
+          fontWeight: `bold`
+        }
+      },
+      reversed: true,
+      startOnTick: false,
+      endOnTick: false,
+      gridLineWidth: 0,
+      minorGridLineWidth: 0
+    },
+    {
+      linkedTo: 0,
+      categories: secondaryYAxisCategories,
+      opposite: true,
+      reversed: true,
+      title: {
+        text: `Cluster ID where marker`,
+        style: {
+          fontWeight: `bold`
+        }
+      },
+      labels: {
+        format: `Cluster {value}`
+      },
+      startOnTick: false,
+      endOnTick: false,
+      gridLineWidth: 0,
+      minorGridLineWidth: 0
+    }],
+
+    tooltip: {
+      formatter: function () {
+        if(this.point.value === null) {
+          return `<b>Cluster ID:</b> ${this.point.x} <br/> <b>Gene ID:</b> ${this.point.name} <br /> <b>Median expression:</b> Not expressed <br/>`
+        }
+        else {
+          return `<b>Cluster ID:</b> ${this.point.x} <br/> <b>Gene ID:</b> ${this.point.name} <br /> <b>Median expression:</b> ${Math.round(this.point.value)} TPM <br/>`
+        }
+      }
+    },
+
+    colorAxis: {
+      type: `logarithmic`,
+      minColor: `#d7ffff`,
+      maxColor: `#024990`,
+      stops: [
+        [0, `#d4e4fb`],
+        [0.67, `#6077bf`],
+        [1, `#0e0573`]
+      ],
+      marker: {
+        color: `#c4463a`
+      }
+    },
+
+    legend: {
+      align: `center`,
+      verticalAlign: `top`,
+      layout: `horizontal`,
+      symbolWidth: 480
+    },
+
+    series: [{
+      data: data,
+      nullColor: `#D3D3D3`,
+      states: {
+        hover: {
+          brightness: 0,
+          borderWidth: 2
+        }
+      },
+      boostThreshold: 100,
+      turboThreshold: 0
+    }],
+
+    boost: {
+      useGPUTranslations: true
+    }
+  }
+
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+    />
+  )
+}
+
+MarkerGeneHeatmap.propTypes = {
+  chartHeight: PropTypes.string,
+  data: PropTypes.arrayOf(PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number,
+    name: PropTypes.string,
+    value: PropTypes.number
+  }))
+}
+
+export default MarkerGeneHeatmap
