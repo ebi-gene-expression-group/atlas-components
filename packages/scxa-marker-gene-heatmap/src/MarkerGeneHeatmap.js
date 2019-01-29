67c0a01de45b4ec62c5f3ffc3c34ed00c3cc3546
Monica Jianu <monica@ebi.ac.uk>
2019-01-29 15:16:06 +0000

    Added plotlines and labels to indicate the separation between marker genes for each cluster. Added more p-value and cluster ID where gene is marker to tooltip. Removed secondary axis. Added plot background colour such that empty cell appear grey rather than white. Added explicit X axis categories.

--- packages/scxa-marker-gene-heatmap/src/MarkerGeneHeatmap.js
+++ packages/scxa-marker-gene-heatmap/src/MarkerGeneHeatmap.js
@@ -6,21 +6,72 @@ import HC_heatmap from 'highcharts/modules/heatmap'
 import _ from 'lodash'
 
 // init the module
-HC_heatmap(Highcharts)
+async function addModules() {
+  HC_heatmap(Highcharts)
+}
+
+addModules()
 
 const MarkerGeneHeatmap = (props) => {
-  const {chartHeight, data} = props
+  const {chartHeight, data, numberOfColumns, hasDynamicHeight, heatmapRowHeight} = props
 
+  const xAxisCategories = Array.from(Array(numberOfColumns+1).keys()).slice(1)
   const yAxisCategories = _.uniq(data.map(x => x.name))
-  const secondaryYAxisCategories = _.uniqBy(data, `name`).map(x => x.clusterIdWhereMarker)
+
+  const groupedData = _.groupBy(data, `clusterIdWhereMarker`)
+  let plotLines = []
+  let plotLineAxisPosition = -0.5
+
+  const clusterIds = Object.keys(groupedData)
+
+  clusterIds.forEach((clusterId, idx, array) => {
+    let numberOfRows = Object.keys(_.groupBy(groupedData[clusterId], `y`)).length // how many marker genes per cluster
+
+    plotLineAxisPosition = plotLineAxisPosition + numberOfRows
+    // Magic number 5 should bring label to the centre the heatmap slice
+    const yOffset = hasDynamicHeight ? -numberOfRows * heatmapRowHeight/2 + 5 : 0
+
+    let color, zIndex
+    // don't show last plot line
+    if (idx === array.length-1) {
+      // removing the plot line altogether would remove the label, so we need to make it "invisible"
+      color = `#FFFFFF`
+      zIndex = 0
+    }
+    else {
+      color = `#000000`
+      zIndex = 5
+    }
+
+    let plotLine = {
+      color: color,
+      // color: '#FFFFFF',
+      width: 2,
+      value: plotLineAxisPosition,
+      zIndex: zIndex,
+      label: {
+        text: `Cluster ${clusterId}`,
+        align: `right`,
+        textAlign: `left`,
+        x: 15,
+        y: yOffset,
+        style: {
+          fontWeight: `bold`,
+          fontSize: `12px`
+        }
+      }
+    }
+    plotLines.push(plotLine)
+  })
 
   const options = {
     chart: {
       type: `heatmap`,
       height: chartHeight,
       zoomType: `y`,
-      animation: false
-      // plotBackgroundColor: `#D3D3D3`
+      animation: false,
+      marginRight: 100,
+      plotBackgroundColor: `#ebebeb`
     },
     credits: {
       enabled: false
@@ -33,16 +84,18 @@ const MarkerGeneHeatmap = (props) => {
       }
     },
     subtitle: {
-      text: `Showing top 50 marker genes per cluster (sorted by p-value)`
+      text: `Showing top 5 marker genes per cluster (sorted by p-value)`
     },
     xAxis: {
       opposite: true,
+      categories: xAxisCategories,
       labels: {
         format: `Cluster {value}`
       },
       endOnTick: false,
       gridLineWidth: 0,
-      minorGridLineWidth: 0
+      minorGridLineWidth: 0,
+      max: numberOfColumns-1
     },
 
     yAxis: [{
@@ -57,35 +110,31 @@ const MarkerGeneHeatmap = (props) => {
       startOnTick: false,
       endOnTick: false,
       gridLineWidth: 0,
-      minorGridLineWidth: 0
+      minorGridLineWidth: 0,
+      plotLines: plotLines
     },
-    {
-      linkedTo: 0,
-      categories: secondaryYAxisCategories,
-      opposite: true,
-      reversed: true,
-      title: {
-        text: `Cluster ID where marker`,
-        style: {
-          fontWeight: `bold`
-        }
-      },
-      labels: {
-        format: `Cluster {value}`
-      },
-      startOnTick: false,
-      endOnTick: false,
-      gridLineWidth: 0,
-      minorGridLineWidth: 0
-    }],
+    ],
 
     tooltip: {
+      // followPointer: true,
       formatter: function () {
         if(this.point.value === null) {
-          return `<b>Cluster ID:</b> ${this.point.x} <br/> <b>Gene ID:</b> ${this.point.name} <br /> <b>Median expression:</b> Not expressed <br/>`
+          return `<b>Cluster ID:</b> ${this.point.x+1} <br/>
+                <b>Gene ID:</b> ${this.point.name} <br/>
+                <b>Median expression:</b> Not expressed <br/>`
         }
         else {
-          return `<b>Cluster ID:</b> ${this.point.x} <br/> <b>Gene ID:</b> ${this.point.name} <br /> <b>Median expression:</b> ${+this.point.value.toFixed(3)} TPM <br/>`
+          const text = `<b>Cluster ID:</b> ${this.point.x+1} <br/> 
+                  <b>Cluster ID where marker:</b> ${this.point.clusterIdWhereMarker} <br/>
+                  <b>Gene ID:</b> ${this.point.name} <br/>
+                  <b>Median expression:</b> ${+this.point.value.toFixed(3)} TPM`
+
+          if(this.point.clusterIdWhereMarker === this.point.x+1) {
+            return text + `<br/><b>P-value:</b> ${this.point.pValue}`
+          }
+          else {
+            return text
+          }
         }
       }
     },
@@ -135,21 +184,26 @@ const MarkerGeneHeatmap = (props) => {
   }
 
   return (
-    <HighchartsReact
-      highcharts={Highcharts}
-      options={options}
-    />
+    <div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+      />
+    </div>
   )
 }
 
 MarkerGeneHeatmap.propTypes = {
-  chartHeight: PropTypes.number,
+  chartHeight: PropTypes.number.required,
+  numberOfColumns: PropTypes.number.required,
   data: PropTypes.arrayOf(PropTypes.shape({
     x: PropTypes.number,
     y: PropTypes.number,
     name: PropTypes.string,
     value: PropTypes.number
-  }))
+  })),
+  hasDynamicHeight: PropTypes.number.required,
+  heatmapRowHeight: PropTypes.number.required
 }
 
 export default MarkerGeneHeatmap
