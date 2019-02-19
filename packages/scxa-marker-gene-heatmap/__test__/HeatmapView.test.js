454a0b4185252c8ec79e8577a9845497e7d01576
Monica Jianu <monica@ebi.ac.uk>
2019-02-19 14:17:35 +0000

    Added additional tests.

--- packages/scxa-marker-gene-heatmap/__test__/HeatmapView.test.js
+++ packages/scxa-marker-gene-heatmap/__test__/HeatmapView.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Enzyme from 'enzyme'
 import {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import fetchMock from 'fetch-mock'
 
 import HeatmapView from '../src/HeatmapView'
 import CalloutAlert from '../src/CalloutAlert'
@@ -9,12 +10,21 @@ import CalloutAlert from '../src/CalloutAlert'
 Enzyme.configure({ adapter: new Adapter() })
 
 describe(`HeatmapView`, () => {
+  beforeEach(() => {
+    fetchMock.restore()
+  })
+
+  const props = {
+    host: `foo/`,
+    resource: `bar`,
+    ks: [1, 2, 3, 4],
+    selectedK: `1`
+  }
+
   test(`Renders error if API request is unsuccessful`, () => {
-    const wrapper = shallow(<HeatmapView
-      host={`foo`}
-      resource={`bar`}
-      ks={[1, 2, 3, 4]}
-      selectedK={`1`}/>)
+    const wrapper = shallow(<HeatmapView {...props} />)
+
+    console.log(wrapper.debug())
 
     expect(wrapper.exists(CalloutAlert)).toBe(true)
   })
