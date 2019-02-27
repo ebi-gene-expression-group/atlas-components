7eb35be168aba8d615e7f6cee0aa697bc2893e58
alfonsomunozpomer <amunoz@ebi.ac.uk>
2019-02-27 16:32:48 +0000

    Fix tests to match new FeedbackButon component

--- packages/atlas-smileys-feedback-form/__test__/FeedbackButton.test.js
+++ packages/atlas-smileys-feedback-form/__test__/FeedbackButton.test.js
@@ -3,27 +3,37 @@ import renderer from 'react-test-renderer'
 import Enzyme from 'enzyme'
 import {shallow, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import Prompt from '../src/Prompt'
-import FeedbackButton from '../src/FeedbackButton.js'
 
-jest.mock("react-ga")
+import Prompt from '../src/Prompt'
+import FeedbackButton from '../src/FeedbackButton'
 
 Enzyme.configure({ adapter: new Adapter() })
 
+jest.mock(`react-ga`)
+
 describe(`FeedbackButton`, () => {
+  const props = {
+    feedbackFormLink:
+      Math.random() > 0.5 ? `https://www.ebi.ac.uk/support/gxa` : `https://www.ebi.ac.uk/support/gxasc`,
+    gaId: `foobar`
+  }
+
   test(`should render a side feedback button`, () => {
-    expect(shallow(<FeedbackButton feedbackFormLink={`linto`} GAid={`id`}/>).find(`#feedback-button`)).toHaveLength(1)
+    expect(mount(<FeedbackButton {...props}/>).find(`button`)).toHaveLength(1)
   })
 
-  test(`should popup a feedback form by clicking a button`, () => {
-    const wrapper = mount(<FeedbackButton feedbackFormLink={`linto`} GAid={`id`}/>)
+  test(`should display a a feedback form by clicking the button`, () => {
+    const wrapper = mount(<FeedbackButton {...props}/>)
     expect(wrapper.find(Prompt)).toHaveLength(0)
     wrapper.find(`button`).simulate('click')
     expect(wrapper.find(Prompt)).toHaveLength(1)
   })
 
   test(`matches snapshot`, () => {
-    const tree = renderer.create(<FeedbackButton feedbackFormLink={`linto`} GAid={`id`}/>).toJSON()
+    const tree = renderer.create(
+      <FeedbackButton
+        feedbackFormLink={`https://www.ebi.ac.uk/support/gxasc`}
+        gaId={`foobar`}/>).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
