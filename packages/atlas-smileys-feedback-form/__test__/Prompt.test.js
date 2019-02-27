28d8ba9d2c5d50375e0e1879d1a086882160387a
alfonsomunozpomer <amunoz@ebi.ac.uk>
2019-02-27 17:17:19 +0000

    Add tests for the revamped Prompt component

--- packages/atlas-smileys-feedback-form/__test__/Prompt.test.js
+++ packages/atlas-smileys-feedback-form/__test__/Prompt.test.js
@@ -5,29 +5,38 @@ import {shallow, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 import Prompt from '../src/Prompt'
-import Emoji from '../src/Emoji'
+import Smiley from '../src/Smiley'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 
 describe(`Prompt`, () => {
+  const props = {
+    feedbackFormLink:
+      Math.random() > 0.5 ? `https://www.ebi.ac.uk/support/gxa` : `https://www.ebi.ac.uk/support/gxasc`,
+    onSelect: () => {}
+  }
 
   test(`should render 5 smiley faces and 1 link to EBI support`, () => {
-    const wrapper = shallow(<Prompt feedbackFormLink={`linto`} onSelect={()=>{}} />)
-    expect(wrapper.find(Emoji)).toHaveLength(5)
+    const wrapper = shallow(<Prompt {...props}/>)
+    expect(wrapper.find(Smiley)).toHaveLength(5)
     expect(wrapper.find(`a`)).toHaveLength(1)
   })
 
-  test(`should display a feedback scale when click smiley faces`, () => {
-    const wrapper = mount(<Prompt feedbackFormLink={`linto`}  onSelect={()=>{}} />)
-    expect(wrapper.find(`#scale`).text()).toBe(`empty`)
+  test(`should store a score when smiley faces are clicked`, () => {
+    const wrapper = mount(<Prompt {...props}/>)
+    expect(wrapper.state(`selectedSmileyScore`)).toBe(0)
 
-    wrapper.find(Emoji).first().simulate(`click`)
-    expect(wrapper.find(`#scale`).text()).toBe(`Terrible`)
+    wrapper.find(Smiley).first().simulate(`click`)
+    expect(wrapper.state(`selectedSmileyScore`)).toBe(1)
+
+    wrapper.find(Smiley).last().simulate(`click`)
+    expect(wrapper.state(`selectedSmileyScore`)).toBe(5)
   })
 
   test(`matches snapshot`, () => {
-    const tree = renderer.create(<Prompt feedbackFormLink={`linto`}  onSelect={()=>{}}/>).toJSON()
+    const tree =
+      renderer.create(<Prompt feedbackFormLink={`https://www.ebi.ac.uk/support/gxasc`}  onSelect={()=>{}}/>).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
