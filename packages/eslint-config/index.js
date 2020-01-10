8a7e78815f482414d5bf6dd7ad7028d0ddc49449
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-01-10 15:23:23 +0000

    📦 eslint-config 💬 Maintenance and update dependencies (#12)
    * JSON-ify config
    
    * Update dependencies and switch to peerDependencies
    
    See https://eslint.org/docs/developer-guide/shareable-configs.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,37 +1,40 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended'
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
   ],
 
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
 
-  env: {
-    'browser': true,
-    'jest': true,
-    'es6': true
+  "env": {
+    "browser": true,
+    "jest": true,
+    "es6": true
   },
 
-  settings: {
-    'import/resolver': 'webpack'
+  "settings": {
+    "import/resolver": "webpack",
+    "react": {
+      "version": "detect"
+    }
   },
 
-  rules: {
-    'no-var': 'error', // Enforce using const and let, rather than var
-    semi: ['warn', 'never'], // Disallow semicolons
-    quotes: ['warn', 'backtick'], // Enforce using `` for strings
-    eqeqeq: ['error', 'always'], // Enforce using === and !==
-    indent: ['error', 2], // Enforce indentation using 2 spaces
-    'no-console': ['warn', { allow: ['warn', 'error']}] // Allow console.warn and console.error statements
+  "rules": {
+    "no-var": "error", // Enforce using const and let, rather than var
+    "semi": ["warn", "never"], // Disallow semicolons
+    "quotes": ["warn", "backtick"], // Enforce using `` for strings
+    "eqeqeq": ["error", "always"], // Enforce using === and !==
+    "indent": ["error", 2], // Enforce indentation using 2 spaces
+    "no-console": ["warn", { "allow": ["warn", "error"] }] // Allow console.warn and console.error statements
   },
-  overrides: [{
-    files: [ '*.test.js' ],
-    rules: {
-      'react/react-in-jsx-scope': 0
+  
+  "overrides": [{
+    "files": ["*.test.js"],
+    "rules": {
+      "react/react-in-jsx-scope": 0
     }
   }]
 }
-
