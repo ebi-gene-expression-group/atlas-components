2f7d956c1f600009fac1167addc24932157ea473
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-02-19 15:43:04 +0000

    Reimplement the header cell for the column to select table rows

---
+++ packages/atlas-experiment-table/__test__/head/SelectionTableHeaderCell.test.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+
+import { Table, Heading } from 'evergreen-ui'
+import ReactTooltip from 'react-tooltip'
+
+import SelectionTableHeaderCell from '../../src/head/SelectionTableHeaderCell'
+
+import { getRandomInt, generateRandomExperimentAccession } from '../TestUtils'
+
+
+//import { getRandomInt } from '../TestUtils'
+import randomString from 'random-string'
+
+describe(`SelectionTableHeaderCell`, () => {
+  const MAX_EXPERIMENT_COUNT = 10
+
+  test(`displays a table header cell with only a label if no rows are selected`, () => {
+    const props = {
+      label: randomString(),
+      selectedRowIds: [],
+      onClick: () => {}
+    }
+    const wrapper = shallow(<SelectionTableHeaderCell {...props}/>)
+
+    expect(wrapper).not.toContainMatchingElement(`a`)
+    expect(wrapper).not.toContainMatchingElement(ReactTooltip)
+
+    expect(wrapper).toContainExactlyOneMatchingElement(Table.HeaderCell)
+    expect(wrapper).toContainExactlyOneMatchingElement(Heading)
+    expect(wrapper.find(Heading)).toHaveProp(`children`, props.label)
+  })
+
+  test(`displays a table header cell with a link if there are rows selected`, () => {
+    const props = {
+      label: randomString(),
+      selectedRowIds:
+          // https://stackoverflow.com/questions/3751520/how-to-generate-sequence-of-numbers-chars-in-javascript
+          Array.apply(0, Array(getRandomInt(1, MAX_EXPERIMENT_COUNT)))
+            .map(() => generateRandomExperimentAccession()),
+      onClick: () => {}
+    }
+    const wrapper = shallow(<SelectionTableHeaderCell {...props}/>)
+
+    expect(wrapper).toContainExactlyOneMatchingElement(Table.HeaderCell)
+    expect(wrapper).toContainExactlyOneMatchingElement(`a`)
+    expect(wrapper.find(`a`)).toIncludeText(props.label)
+  })
+
+  test(`calls onClick with the selected row IDs when the link is clicked`, () => {
+    const props = {
+      label: randomString(),
+      selectedRowIds:
+          // https://stackoverflow.com/questions/3751520/how-to-generate-sequence-of-numbers-chars-in-javascript
+          Array.apply(0, Array(getRandomInt(1, MAX_EXPERIMENT_COUNT)))
+            .map(() => generateRandomExperimentAccession()),
+      onClick: jest.fn()
+    }
+    const wrapper = shallow(<SelectionTableHeaderCell {...props}/>)
+
+    wrapper.find(`a`).simulate(`click`)
+    expect(props.onClick).toHaveBeenCalled()
+    expect(props.onClick.mock.calls).toContainEqual([props.selectedRowIds])
+  })
+
+  test(`can display an optional tooltip`, () => {
+    const props = {
+      label: randomString(),
+      selectedRowIds: [],
+      onClick: () => {},
+      tooltipContent: randomString({length: 100})
+    }
+    const wrapper = shallow(<SelectionTableHeaderCell {...props}/>)
+
+    expect(wrapper).toContainExactlyOneMatchingElement(ReactTooltip)
+  })
+
+  test(`matches snapshot (basic)`, () => {
+    const props = {
+      label: `Action`,
+      selectedRowIds: [],
+      onClick: () => {}
+    }
+
+    expect(mount(<SelectionTableHeaderCell {...props}/>)).toMatchSnapshot()
+  })
+
+  test(`matches snapshot (with a selection)`, () => {
+    const props = {
+      label: `Action`,
+      selectedRowIds: [`E-EHCA-1`, `E-MTAB-5200`],
+      onClick: jest.fn()
+    }
+
+
+    expect(mount(<SelectionTableHeaderCell {...props}/>)).toMatchSnapshot()
+  })
+
+  test(`matches snapshot (with a selection and a tooltip)`, () => {
+    const props = {
+      label: `Action`,
+      selectedRowIds: [`E-EHCA-1`, `E-MTAB-5200`],
+      onClick: jest.fn(),
+      tooltipContent: `Detailed description of the action performed on the rows when the header cell link is clicked`
+    }
+
+    expect(mount(<SelectionTableHeaderCell {...props}/>)).toMatchSnapshot()
+  })
+})
---
+++ packages/atlas-experiment-table/src/head/SelectionTableHeaderCell.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// We’re not using Link because Evergreen restyles the links and we want EBI VF for that
+// We also don’t like Evergreen’s Tooltip because  it doesn’t render HTML :(
+import { Table, Heading, Pane, majorScale } from 'evergreen-ui'
+import ReactTooltip from 'react-tooltip'
+
+const TooltipIcon = ({tooltipText}) =>
+  <sup>
+    <span
+      data-tip={`${tooltipText}`}
+      data-html={true}
+      className={`icon icon-generic`} data-icon={`i`}
+      style={{color: `lightgrey`, fontSize: `small`}}/>
+  </sup>
+
+TooltipIcon.propTypes = {
+  tooltipText: PropTypes.string.isRequired
+}
+
+const SelectionTableHeaderCell = ({ label, selectedRowIds, onClick, tooltipContent }) =>
+  <Table.HeaderCell
+    justifyContent={`center`}>
+    <Heading size={500}>
+      {
+        selectedRowIds.length > 0 ?
+          <a onClick={() => onClick(selectedRowIds)}>
+            {`${label} ${selectedRowIds.length} ${selectedRowIds.length === 1 ? `entry` : `entries`}`}
+          </a> :
+          label
+      }
+    </Heading>
+    { tooltipContent &&
+      <Pane paddingLeft={majorScale(1)}>
+        <TooltipIcon tooltipText={tooltipContent}/>
+        <ReactTooltip place={`bottom`}/>
+      </Pane>
+    }
+  </Table.HeaderCell>
+
+SelectionTableHeaderCell.propTypes = {
+  label: PropTypes.string.isRequired,
+  selectedRowIds: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onClick: PropTypes.func.isRequired,
+  tooltipContent: PropTypes.string,
+}
+
+export default SelectionTableHeaderCell
