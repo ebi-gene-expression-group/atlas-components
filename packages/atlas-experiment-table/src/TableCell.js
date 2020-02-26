13663e9979514861dd38e366e3e573f93fcdb042
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-02-26 14:00:54 +0000

    Add a TableCell component to encapsulate regular rows contents

---
+++ packages/atlas-experiment-table/src/TableCell.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import URI from 'urijs'
+
+import { Table, Text } from 'evergreen-ui'
+
+const TableCell = ({ dataRow, dataKey, image, linkTo, host, width }) => {
+  // If the column is an image put the image in the cell (or an unknown icon if the type is undefined)
+  const cellItem = image ?
+    image[dataRow[dataKey]] ?
+      <img {...image[dataRow[dataKey]]}/> :
+      `❔` :
+
+    // If the contents of the cell is an array expand it to a list
+    Array.isArray(dataRow[dataKey]) ?
+      <ul>
+        {dataRow[dataKey].map(
+          (element, index) =>
+            <li key={`${index}`}>
+              {element}
+            </li>
+        )}
+      </ul> :
+      // Any other type (i.e. string, number) put it as-is in the cell
+      dataRow[dataKey]
+
+  // Evergreen’s Table.TextCell ellipsifies content by default and we don’t want that
+  return (
+    <Table.Cell flexGrow={width}>
+      <Text size={400}>
+        {
+          linkTo ?
+            <a href={URI(linkTo(dataRow), host).toString()}>{cellItem}</a> :
+            cellItem
+        }
+      </Text>
+    </Table.Cell>
+  )
+}
+
+TableCell.propTypes = {
+  dataRow: PropTypes.object.isRequired,
+  dataKey: PropTypes.string.isRequired,
+  image: PropTypes.objectOf(
+    PropTypes.shape({
+      src: PropTypes.string,
+      alt: PropTypes.string,
+      title: PropTypes.string
+    })
+  ),
+  host: PropTypes.string,
+  linkTo: PropTypes.func,
+  width: PropTypes.number,
+}
+
+TableCell.defaultProps = {
+  image: null,
+  linkTo: null,
+  host: ``,
+  width: 1,
+}
+
+export default TableCell
