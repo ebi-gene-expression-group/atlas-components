02e93725e86d91d4527339384c780238385e2327
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-03-05 11:45:19 +0000

    Use styled-components in LoadingOverlay and use flex-direction

--- packages/scxa-tsne-plot/src/plotloader/LoadingOverlay.js
+++ packages/scxa-tsne-plot/src/plotloader/LoadingOverlay.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import URI from 'urijs'
 
 import flaskLoaderSvg from './svg/flask-loader.svg'
 
-const LoadingOverlay = (props) =>
-  <div style={{
-    display: props.show ? `flex` : `none`,
-    position: `absolute`,
-    top: 0, left: 0, right: 0, bottom: 0,
-    background: `rgba(255,255,255,0.8)`,
-    alignItems: `center`,
-    justifyContent: `center`
-  }}>
-    <div style={{textAlign: `center`}}>
-      <p>Loading, please wait...</p>
-      <img src={URI(flaskLoaderSvg, props.resourcesUrl).toString()}/>
-    </div>
-  </div>
+const TranslucentOverlay = styled.div`
+  display: ${props => props.show ? `flex` : `none`};
+  position: absolute;
+  top: 0; left: 0; right: 0; bottom: 0;
+  background: rgba(255,255,255,0.8);
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+ `
+
+const LoadingOverlay = ({show, resourcesUrl}) =>
+  <TranslucentOverlay show={show}>
+    <p>Loading, please wait...</p>
+    <img src={URI(flaskLoaderSvg, resourcesUrl).toString()}/>
+  </TranslucentOverlay>
 
 LoadingOverlay.propTypes = {
   show: PropTypes.bool.isRequired,
