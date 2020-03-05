d15b511ea6f9db6f357a15aac4c62bc49603710d
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-03-05 14:20:27 +0000

    Remove snapshot tests (!) and do proper seed-based tests

--- packages/highcharts-series-generator/__test__/HighchartsSeriesGenerator.test.js
+++ packages/highcharts-series-generator/__test__/HighchartsSeriesGenerator.test.js
@@ -1,19 +1,25 @@
 import HighchartsSeriesGenerator from '../src/HighchartsSeriesGenerator'
 
+// Stolen from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// const getRandomInt = (min, max) => {
+//   min = Math.ceil(min)
+//   max = Math.floor(max)
+//   return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive
+// }
+
 describe (`HighchartsSeriesGenerator`, () => {
   const seriesNames = [`Series 1`, `Series 2`, `Series 3`, `Series 4`, `Series 5`]
   const maxPointsPerSeries = 1000
 
   test(`creates the right number of series`, () => {
-    const randomSeriesNames =
-      seriesNames.reduce((acc, seriesName) => Math.random() > 0.5 ? acc.concat(seriesName) : acc, [])
+    const randomSeriesNames = seriesNames.filter(() => Math.random() < 0.5)
 
-    expect(HighchartsSeriesGenerator.generate(randomSeriesNames, maxPointsPerSeries)).toHaveLength(randomSeriesNames.length)
+    expect(HighchartsSeriesGenerator.generate(randomSeriesNames, maxPointsPerSeries))
+      .toHaveLength(randomSeriesNames.length)
   })
 
   test(`creates series with the right format, as per Highcharts API, plus a field for gene expression`, () => {
-    const randomSeriesNames =
-      seriesNames.reduce((acc, seriesName) => Math.random() > 0.5 ? acc.concat(seriesName) : acc, [])
+    const randomSeriesNames = seriesNames.filter(() => Math.random() < 0.5)
 
     HighchartsSeriesGenerator.generate(randomSeriesNames, maxPointsPerSeries).forEach((series) => {
       expect(series).toHaveProperty(`name`)
@@ -28,30 +34,24 @@ describe (`HighchartsSeriesGenerator`, () => {
   })
 
   test(`data points are bounded by parameter`, () => {
-    const randomSeriesNames =
-      seriesNames.reduce((acc, seriesName) => Math.random() > 0.5 ? acc.concat(seriesName) : acc, [])
+    const randomSeriesNames = seriesNames.filter(() => Math.random() < 0.5)
 
-    HighchartsSeriesGenerator.generate(randomSeriesNames, maxPointsPerSeries).forEach((series) => {
-      expect(series.data.length).toBeLessThanOrEqual(maxPointsPerSeries)
-    })
+    HighchartsSeriesGenerator.generate(randomSeriesNames, maxPointsPerSeries)
+      .forEach((series) => {
+        expect(series.data.length).toBeLessThanOrEqual(maxPointsPerSeries)
+      })
   })
 
-  test(`data with the same seed`, () => {
-    const randomSeriesNames =
-      seriesNames.reduce((acc, seriesName) => Math.random() > 0.5 ? acc.concat(seriesName) : acc, [])
-
-    expect(HighchartsSeriesGenerator.generate(randomSeriesNames, maxPointsPerSeries)).toHaveLength(randomSeriesNames.length)
-  })
-
-  test(`series generated with the same seed match snapshot`, () => {
+  test(`data with the same seed match, and not otherwise`, () => {
     const seed = `If you get killed in someone else’s dream, you die for real, Morty.`
-    const series = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries, seed)
-    expect(series).toMatchSnapshot()
-  })
 
-  // Jest: `.not` cannot be used with `.toMatchSnapshot()`.
-  test(`series generated without a seed don’t match snapshot`, () => {
-    const series = HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries)
-    expect(series).toThrowErrorMatchingSnapshot()
+    expect(HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries, seed))
+      .toEqual(HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries, seed))
+
+    expect(HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries, seed))
+      .not.toEqual(HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries))
+
+    expect(HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries))
+      .not.toEqual(HighchartsSeriesGenerator.generate(seriesNames, maxPointsPerSeries))
   })
 })
