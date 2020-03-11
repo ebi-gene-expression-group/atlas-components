c7c88b4628e465f3bb572c03380bb50e783c9ff1
amunozpomer <amunoz@ebi.ac.uk>
2020-03-11 11:20:44 +0000

    Move BrowserRouter component to index.js and remove TableManagerSpa

--- packages/atlas-experiment-table/src/index.js
+++ packages/atlas-experiment-table/src/index.js
@@ -1,2 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import TableManagerRoute from './TableManagerRoute'
+
+const TableManagerRouter = (props) =>
+  <BrowserRouter basename={props.basename}>
+    <TableManagerRoute {...props}/>
+  </BrowserRouter>
+
+TableManagerRouter.propTypes = {
+  basename: PropTypes.string
+}
+
+TableManagerRouter.defaultProps = {
+  basename: ``
+}
+
 export { default as TableManager } from './TableManager'
-export { default as TableManagerSpa } from './TableManagerSpa'
+export { TableManagerRouter }
