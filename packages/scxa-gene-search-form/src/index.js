65e8ff6ab0dc516934eae3541ea526e7c2612312
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-03-25 10:39:33 +0000

    Add convenience render method so we can get rid off the bundle in atlas-web-single-cell

--- packages/scxa-gene-search-form/src/index.js
+++ packages/scxa-gene-search-form/src/index.js
@@ -1,3 +1,11 @@
-import FetchLoader from './FetchLoader.js'
 
-export default FetchLoader
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import GeneSearchFormWithFetchLoader from './FetchLoader'
+
+const render = (options, target) => {
+  ReactDOM.render(<GeneSearchFormWithFetchLoader {...options} />, document.getElementById(target))
+}
+
+export { GeneSearchFormWithFetchLoader as default, render }
