831f504c9f7b44e088e5c15d173803fd50a4fe21
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-04-30 19:06:08 +0100

    Remove `act` and tidy up

--- packages/atlas-experiment-table/__test__/download/ExperimentDownloadModule.test.js
+++ packages/atlas-experiment-table/__test__/download/ExperimentDownloadModule.test.js
@@ -1,7 +1,6 @@
 // Even if we pass a callback mock to loadExperimentDownloadModule, validateAndDownloadExperimentFiles is imported in
 // the module itself, so we need Babel’s polyfill for fetch
 import '@babel/polyfill'
-import { act } from 'react-dom/test-utils'
 
 import loadExperimentDownloadModule from '../../src/download/ExperimentDownloadModule'
 
@@ -17,8 +16,7 @@ describe(`ExperimentDownloadModule`, () => {
     container = document.createElement(`div`)
     container.setAttribute(`id`, popupPlaceholderId)
     document.body.appendChild(container)
-    openDownloadPopup =
-      loadExperimentDownloadModule(host, downloadFileTypes, popupPlaceholderId, callbackMock)
+    openDownloadPopup = loadExperimentDownloadModule(host, downloadFileTypes, popupPlaceholderId, callbackMock)
   })
 
   beforeEach(() => {
@@ -36,9 +34,8 @@ describe(`ExperimentDownloadModule`, () => {
     expect(container.querySelector(`.mm-popup--visible`)).toBeNull()
     expect(container.querySelector(`button`)).toBeNull()
 
-    act(() => {
-      openDownloadPopup()
-    })
+    openDownloadPopup()
+    
     expect(container.querySelector(`.mm-popup`)).not.toBeNull()
     expect(container.querySelector(`.mm-popup--visible`)).not.toBeNull()
     // Close (not visible), cancel and success buttons;
@@ -48,27 +45,18 @@ describe(`ExperimentDownloadModule`, () => {
   })
 
   test(`displays a pop-up that can be dismissed with the second (i.e. Cancel) button`, () => {
-    act(() => {
-      openDownloadPopup()
-    })
+    openDownloadPopup()
     expect(container.querySelector(`.mm-popup--visible`)).not.toBeNull()
 
-    act(() => {
-      container.querySelectorAll(`button`)[1].dispatchEvent(new MouseEvent(`click`, {bubbles: true}))
-    })
+    container.querySelectorAll(`button`)[1].dispatchEvent(new MouseEvent(`click`, {bubbles: true}))
     expect(container.querySelector(`.mm-popup--visible`)).toBeNull()
   })
 
   test(`runs the callback argument when the success (i.e. Download) button is clicked`, async () => {
     const experimentAccession = generateRandomExperimentAccession()
-    act(() => {
-      openDownloadPopup([experimentAccession])
-    })
-    expect(container.querySelector(`.mm-popup--visible`)).not.toBeNull()
 
-    act(() => {
-      container.querySelectorAll(`button`)[2].dispatchEvent(new MouseEvent(`click`, {bubbles: true}))
-    })
+    openDownloadPopup([experimentAccession])
+    container.querySelectorAll(`button`)[2].dispatchEvent(new MouseEvent(`click`, {bubbles: true}))
 
     expect(callbackMock.mock.calls.length).toBe(1)
     expect(callbackMock.mock.calls[0][0]).toBe(host)
@@ -81,17 +69,11 @@ describe(`ExperimentDownloadModule`, () => {
   // empty array in the download filetypes argument. This is a situation that won’t happen if the module is used as
   // intended but it serves our test case.
   test(`does nothing if no filetypes are selected`, async () => {
-    openDownloadPopup =
-      loadExperimentDownloadModule(host, [], popupPlaceholderId, callbackMock)
+    openDownloadPopup = loadExperimentDownloadModule(host, [], popupPlaceholderId, callbackMock)
     const experimentAccession = generateRandomExperimentAccession()
-    act(() => {
-      openDownloadPopup([experimentAccession])
-    })
-    expect(container.querySelector(`.mm-popup--visible`)).not.toBeNull()
 
-    act(() => {
-      container.querySelectorAll(`button`)[2].dispatchEvent(new MouseEvent(`click`, {bubbles: true}))
-    })
+    openDownloadPopup([experimentAccession])
+    container.querySelectorAll(`button`)[2].dispatchEvent(new MouseEvent(`click`, {bubbles: true}))
 
     expect(callbackMock.mock.calls.length).toBe(0)
   })
