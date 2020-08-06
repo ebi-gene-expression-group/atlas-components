3da62998ac835941e0e4c031ed8227961d454dee
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2020-08-06 13:24:06 +0100

    Fix demo page

--- packages/scxa-gene-search-form/html/render.js
+++ packages/scxa-gene-search-form/html/render.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import GeneSearchForm from '../src/index'
 import LabelledSelect from '../src/LabelledSelect'
-
-const render = (options, target) => {
-  ReactDOM.render(<GeneSearchForm {...options} />, document.getElementById(target))
-}
 
 const renderLabelledSelect = (options, target) => {
   ReactDOM.render(<LabelledSelect {...options} />, document.getElementById(target))
 }
 
-export { render, renderLabelledSelect }
+export { render } from '../src/index'
+export { renderLabelledSelect }
