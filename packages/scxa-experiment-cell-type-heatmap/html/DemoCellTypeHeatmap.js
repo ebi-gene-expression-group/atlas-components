03c740af66786ae3d8550b8478ccda6db03936a9
amunozpomer <amunoz@ebi.ac.uk>
2020-10-17 11:52:50 +0100

    Pull data from back end for the demo page

--- packages/scxa-experiment-cell-type-heatmap/html/DemoCellTypeHeatmap.js
+++ packages/scxa-experiment-cell-type-heatmap/html/DemoCellTypeHeatmap.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom'
 
 import HeatmapView from '../src/index.js'
 
-const testJSON =
-`https://gist.githubusercontent.com/lingyun1010/3f75f1815890739a50300d76b471167d/raw/07ccd0cfdefdb4829a3ea536b41a6d76d59d4478/gistfile1.json`
-const testJSON2 = "https://gist.githubusercontent.com/lingyun1010/065426db1011c1a080d58cf0f18bb872/raw/c7ac88d119db912433daf302efa5ed23acc1f43f/cellTypeWheelJsonPayload.json"
-
 const props = {
-  resource: ``,
-  host: testJSON,
-  type:`cellType`,
+  resource: `json/experiments/E-MTAB-5061/marker-genes/cell-types?organismPart=http://purl.obolibrary.org/obo/UBERON_0001264`,
+  host: `http://localhost:8080/gxa/sc/`,
   props: {
     species: `Homo sapiens`,
     hasDynamicHeight: true,
