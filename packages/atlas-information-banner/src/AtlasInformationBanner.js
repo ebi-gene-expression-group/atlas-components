4af139037f07393f476ab388db94a79c4fd2a43c
amunozpomer <amunoz@ebi.ac.uk>
2021-02-25 17:08:00 +0000

    First iteration with static text and link and dismiss/close button

--- packages/atlas-information-banner/src/AtlasInformationBanner.js
+++ packages/atlas-information-banner/src/AtlasInformationBanner.js
@@ -4,17 +4,31 @@ import PropTypes from 'prop-types'
 class AtlasInformationBanner extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      visibility: ``
+    }
   }
 
   render() {
     return(
-      <div className={`foo`}><p>This is an information message</p></div>
+      <div className={`row`} style={{display: this.state.visibility}}>
+        <div className={`columns small-11`}>
+          <p>
+            <b>We need your help!</b>
+          </p>
+          <p>
+            If you’ve ever found our data helpful, please take our impact survey (15 min). Your replies will
+            help keep the data flowing to the scientific community.
+          </p>
+          <a target={`_blank`} href={`https://www.surveymonkey.co.uk/r/EMBL-EBI_Impact_DR`} className={`button`}>Take survey</a>
+        </div>
+        <div className={`columns small-1`}>
+          <a href={`#`} onClick={ () => this.setState({visibility: `none`}) } style={{fontSize:`xx-large`, border: `none`}}>&times;</a>
+        </div>
+      </div>
     )
   }
-}
-
-AtlasInformationBanner.propTypes = {
-  atlasUrl: PropTypes.string
 }
 
 export default AtlasInformationBanner
