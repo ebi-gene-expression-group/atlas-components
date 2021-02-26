f573942d69b778162501562d1eed4648244f383b
upendra kumbham <ukumbham@ebi.ac.uk>
2021-02-26 14:39:05 +0000

    add a functional test

--- packages/atlas-information-banner/__test__/AtlasInformationBanner.test.js
+++ packages/atlas-information-banner/__test__/AtlasInformationBanner.test.js
@@ -1,29 +1,20 @@
 // If you get ReferenceError: regeneratorRuntime is not defined use this in your tests:
 // import '@babel/polyfill'
 import renderer from 'react-test-renderer'
-import {shallow, mount, render} from 'enzyme'
+import {shallow} from 'enzyme'
 
 import AtlasInformationBanner from '../src/AtlasInformationBanner'
 
 describe(`AtlasInformationBanner`, () => {
-  test(`should render without throwing an error`, () => {
-    expect(shallow(<AtlasInformationBanner />).contains(<div className="foo">Bar</div>)).toBe(true)
-  })
+	test(`disappears when close button is clicked`, () => {
+		const wrapper = shallow(<AtlasInformationBanner/>)
+		expect(wrapper.state('visibility')).toHaveLength(0);
+		wrapper.find('a').last().simulate('click')
+		expect(wrapper.state('visibility')).toBe('none');
+	})
 
-  test(`should be selectable by class "foo"`, () => {
-    expect(shallow(<AtlasInformationBanner />).is(`.foo`)).toBe(true)
-  })
-
-  test(`should mount in a full DOM`, () => {
-    expect(mount(<AtlasInformationBanner />).find(`.foo`)).toHaveLength(1)
-  })
-
-  test(`should render to static HTML`, () => {
-    expect(render(<AtlasInformationBanner />).text()).toEqual(`Bar`)
-  })
-
-  test(`matches snapshot`, () => {
-    const tree = renderer.create(<AtlasInformationBanner />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+	test(`matches snapshot`, () => {
+		const tree = renderer.create(<AtlasInformationBanner/>).toJSON()
+		expect(tree).toMatchSnapshot()
+	})
 })
