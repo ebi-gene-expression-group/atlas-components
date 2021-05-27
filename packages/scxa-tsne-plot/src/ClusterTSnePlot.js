b98ec6f1e80ba7277eff6853f53155d9824687f3
lingyun <zhaolingyun1010@gmail.com>
2021-05-27 09:17:26 +0100

    Refactor ClusterTSnePlot as a unstateful component

--- packages/scxa-tsne-plot/src/ClusterTSnePlot.js
+++ packages/scxa-tsne-plot/src/ClusterTSnePlot.js
@@ -34,200 +34,156 @@ const tooltipHeader = (clusterType, series, point) => {
   return `<b>Cell ID:</b> ${point.name}<br>` + clusterName
 }
 
-class ClusterTSnePlot extends React.Component {
-  constructor(props) {
-    super(props)
+const ClusterTSnePlot = (props) => {
+  const {ks, metadata, selectedColourBy, onChangeColourBy, clusterType} = props  // Select
+  const {plotData, highlightClusters, height, tooltipContent} = props   // Chart
+  const {loading, resourcesUrl, errorMessage} = props   // Overlay
 
-    this.state = {
+  const opacity = 0.7
+
+  const highchartsConfig = {
+    plotOptions: {
+      scatter: {
+        marker: {
+          symbol: `circle`
+        }
+      }
+    },
+    // Generated with http://tools.medialab.sciences-po.fr/iwanthue/
+    colors: [
+      `rgba(212, 137, 48, ${opacity})`,
+      `rgba(71, 193, 152, ${opacity})`,
+      `rgba(90, 147, 221, ${opacity})`,
+      `rgba(194, 73, 97, ${opacity})`,
+      `rgba(128, 177, 66, ${opacity})`,
+      `rgba(208, 76, 134, ${opacity})`,
+      `rgba(188, 176, 59, ${opacity})`,
+      `rgba(132, 43, 102, ${opacity})`,
+      `rgba(93, 188, 108, ${opacity})`,
+      `rgba(82, 45, 128, ${opacity})`,
+      `rgba(101, 133, 52, ${opacity})`,
+      `rgba(169, 107, 212, ${opacity})`,
+      `rgba(185, 140, 70, ${opacity})`,
+      `rgba(82, 88, 180, ${opacity})`,
+      `rgba(176, 73, 62, ${opacity})`,
+      `rgba(101, 127, 233, ${opacity})`,
+      `rgba(214, 126, 188, ${opacity})`,
+      `rgba(196, 86, 178, ${opacity})`,
+      `rgba(173, 131, 211, ${opacity})`,
+      `rgba(193, 84, 47, ${opacity})`
+    ],
+    chart: {
+      height: height
+    },
+    title: {
+      text: `Clusters`
+    },
+    legend: {
+      enabled: true,
+      align: `center`,
+      verticalAlign: `bottom`,
+      layout: `horizontal`,
+      itemMarginBottom: 20
+    },
+    tooltip: {
+      formatter: function (tooltip) {
+        // Trick Highcharts into thinking the point is in the bottom half of the chart, so that the tooltip
+        // is displayed below the point
+        this.point.negative = true
+        const text = `Loading metadata...`
+        const header = tooltipHeader(clusterType, this.series, this.point)
+        const addMetadataToTooltipText = async () => {
+          try {
+            const response = await tooltipContent(this.point.name)
+
+            const content = response.map((metadata) => {
+              return `<b>${metadata.displayName}:</b> ${metadata.value}`
+            })
+
+            tooltip.label.attr({
+              text: header + content.join(`<br>`)
+            })
+          } catch (e) {
+            tooltip.label.attr({
+              text: header
+            })
+          }
+        }
+
+        addMetadataToTooltipText()
+
+        return header + text
+      }
     }
   }
 
-  render() {
-    const {ks, metadata, selectedColourBy, onChangeColourBy, clusterType, onChangePlotOptions,
-      onChangePlotTypes, plotTypeDropdown, selectedPlotType, selectedParameter} = this.props  // Select
-    const {plotData, highlightClusters, height, tooltipContent} = this.props   // Chart
-    const {loading, resourcesUrl, errorMessage, showControls} = this.props   // Overlay
+  const kOptions = ks.sort((a, b) => a - b).map((k) => ({
+    value: k.toString(),
+    label: `k = ${k}`,
+    group: `clusters`
+  }))
 
-    const plot = _find(plotTypeDropdown, (plot) => plot.plotType.toLowerCase() === selectedPlotType)
+  const metadataOptions = metadata.map((metadata) => ({
+    ...metadata,
+    group: `metadata`
+  }))
 
-    const plotOptions = plot.plotOptions
-    const plotOptionsValues = plotOptions.map((value) => ({value: value, label: value}))
-    const plotOptionsLabel = plot.plotOptionsLabel
-    const plotTypesOptions = plotTypeDropdown.map((plot) => ({
-      value: plot.plotType.toLowerCase(),
-      label: plot.plotType
-    }))
+  const options = [
+    {
+      label: `Metadata`,
+      options: metadataOptions,
+    },
+    {
+      label: `Number of clusters`,
+      options: kOptions,
+    },
+  ]
 
-    const opacity = 0.7
-
-    const highchartsConfig = {
-      plotOptions: {
-        scatter: {
-          marker: {
-            symbol: `circle`
-          }
-        }
-      },
-      // Generated with http://tools.medialab.sciences-po.fr/iwanthue/
-      colors: [
-        `rgba(212, 137, 48, ${opacity})`,
-        `rgba(71, 193, 152, ${opacity})`,
-        `rgba(90, 147, 221, ${opacity})`,
-        `rgba(194, 73, 97, ${opacity})`,
-        `rgba(128, 177, 66, ${opacity})`,
-        `rgba(208, 76, 134, ${opacity})`,
-        `rgba(188, 176, 59, ${opacity})`,
-        `rgba(132, 43, 102, ${opacity})`,
-        `rgba(93, 188, 108, ${opacity})`,
-        `rgba(82, 45, 128, ${opacity})`,
-        `rgba(101, 133, 52, ${opacity})`,
-        `rgba(169, 107, 212, ${opacity})`,
-        `rgba(185, 140, 70, ${opacity})`,
-        `rgba(82, 88, 180, ${opacity})`,
-        `rgba(176, 73, 62, ${opacity})`,
-        `rgba(101, 127, 233, ${opacity})`,
-        `rgba(214, 126, 188, ${opacity})`,
-        `rgba(196, 86, 178, ${opacity})`,
-        `rgba(173, 131, 211, ${opacity})`,
-        `rgba(193, 84, 47, ${opacity})`
-      ],
-      chart: {
-        height: height
-      },
-      title: {
-        text: `Clusters`
-      },
-      legend: {
-        enabled: true,
-        align: `center`,
-        verticalAlign: `bottom`,
-        layout: `horizontal`,
-        itemMarginBottom: 20
-      },
-      tooltip: {
-        formatter: function (tooltip) {
-          // Trick Highcharts into thinking the point is in the bottom half of the chart, so that the tooltip
-          // is displayed below the point
-          this.point.negative = true
-          const text = `Loading metadata...`
-          const header = tooltipHeader(clusterType, this.series, this.point)
-          const addMetadataToTooltipText = async () => {
-            try {
-              const response = await tooltipContent(this.point.name)
-
-              const content = response.map((metadata) => {
-                return `<b>${metadata.displayName}:</b> ${metadata.value}`
-              })
-
-              tooltip.label.attr({
-                text: header + content.join(`<br>`)
-              })
-            } catch (e) {
-              tooltip.label.attr({
-                text: header
-              })
-            }
-          }
-
-          addMetadataToTooltipText()
-
-          return header + text
-        }
-      }
-    }
-
-    const kOptions = ks.sort((a, b) => a - b).map((k) => ({
-      value: k.toString(),
-      label: `k = ${k}`,
-      group: `clusters`
-    }))
-
-    const metadataOptions = metadata.map((metadata) => ({
-      ...metadata,
-      group: `metadata`
-    }))
-
-    const options = [
-      {
-        label: `Metadata`,
-        options: metadataOptions,
-      },
-      {
-        label: `Number of clusters`,
-        options: kOptions,
-      },
-    ]
-
-    const defaultValue = _find(
+  const defaultValue = _find(
       _flatten(
-        options.map((item) => (item.options))
+          options.map((item) => (item.options))
       ),
       {value: selectedColourBy}
-    )
+  )
 
-    const afterRender = (chart) => {
-      chart.series.forEach(series => {
-        const legendSymbolSize = 10
-        series.legendSymbol.translate(
+  const afterRender = (chart) => {
+    chart.series.forEach(series => {
+      const legendSymbolSize = 10
+      series.legendSymbol.translate(
           (series.legendSymbol.width / 2) - legendSymbolSize / 2,
           (series.legendSymbol.height / 2) - legendSymbolSize / 2)
-        series.legendSymbol.attr(`width`, legendSymbolSize)
-        series.legendSymbol.attr(`height`, legendSymbolSize)
-      })
-    }
+      series.legendSymbol.attr(`width`, legendSymbolSize)
+      series.legendSymbol.attr(`height`, legendSymbolSize)
+    })
+  }
 
-    return (
+  return (
       <React.Fragment>
         <div key={`perplexity-k-select`} className={`row`}>
-          {showControls &&
-<div>
-        <div className={`small-12 medium-3 columns`}>
           <PlotSettingsDropdown
-            labelText={`Plot Types`}
-            options={plotTypesOptions}
-            defaultValue={{value: selectedPlotType, label: plot.plotType}}
-            onSelect={(selectedPlotType) => {
-              onChangePlotTypes(selectedPlotType.value)
-            }}/>
-        </div>
-          <div className={`small-12 medium-3 columns`}>
-          <PlotSettingsDropdown
-            labelText={plotOptionsLabel}
-            options={plotOptionsValues}
-            defaultValue={{value: selectedParameter, label: selectedParameter}}
-            onSelect={(selectedOption) => {
-              onChangePlotOptions(selectedOption.value)
-            }}/>
-            </div>
-  </div>
-          }
-          <div className={`small-12 medium-3 columns`}>
-            <PlotSettingsDropdown
               labelText={`Colour plot by:`}
               options={metadata ? options : kOptions} // Some experiments don't have metadata in Solr, although they should do. Leaving this check in for now so we don't break the entire experiment page.
               defaultValue={defaultValue}
               onSelect={(selectedOption) => {
                 onChangeColourBy(selectedOption.group, selectedOption.value)
               }}/>
-          </div>
         </div>
         <ScatterPlotLoader
-          key={`cluster-plot`}
-          wrapperClassName={`row`}
-          chartClassName={`small-12 columns`}
-          series={_colourizeClusters(highlightClusters)(plotData.series)}
-          highchartsConfig={highchartsConfig}
-          loading={loading}
-          resourcesUrl={resourcesUrl}
-          errorMessage={errorMessage}
-          afterRender={afterRender}
+            key={`cluster-plot`}
+            wrapperClassName={`row`}
+            chartClassName={`small-12 columns`}
+            series={_colourizeClusters(highlightClusters)(plotData.series)}
+            highchartsConfig={highchartsConfig}
+            loading={loading}
+            resourcesUrl={resourcesUrl}
+            errorMessage={errorMessage}
+            afterRender={afterRender}
         />
       </React.Fragment>
-    )
-  }
-
-
+  )
 }
+
+
 
 ClusterTSnePlot.propTypes = {
   height: PropTypes.number.isRequired,
