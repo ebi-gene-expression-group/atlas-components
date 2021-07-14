f3dc55446d37738b47dd667d280013f7ba74c289
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-07-14 11:13:04 +0100

    📦 react-ebi-species 💬 v3.0.0 (#78)
    * Update to React 17 and ditch Enzyme :(
    
    * Update Webpack config to v5 and dev server to v4
    
    * Replace Enzyme with Testing-Library
    
    * Replace snapshots with React Test Renderer
    
    * Add encodingo to HTML demo pages
    
    * Lock packages

--- packages/react-ebi-species/__test__/EbiSpeciesIcon.test.js
+++ packages/react-ebi-species/__test__/EbiSpeciesIcon.test.js
@@ -1,7 +1,17 @@
-import { shallow, mount } from 'enzyme'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { cleanup, render, screen } from '@testing-library/react'
 
 import { lookUpIcon, allSpecies } from '../src/mapping'
 import EbiSpeciesIcon from '../src/EbiSpeciesIcon'
+
+const strToRegExp = str =>
+  new RegExp(
+    str
+      .replace(/\(/g, `\\(`)
+      .replace(/\)/g, `\\)`),
+    `i`
+  )
 
 const altGroupColors = {
   mammals: `brown`,
@@ -12,59 +22,95 @@ const altGroupColors = {
 describe(`EbiSpeciesIcon with classes`, () => {
   allSpecies.forEach(species => {
     test(`Icon of mapped species ${species} matches snapshot`, () => {
-      expect(mount(<EbiSpeciesIcon species={species} />)).toMatchSnapshot()
+      const tree = renderer
+        .create(<EbiSpeciesIcon species={species} />)
+        .toJSON()
+      expect(tree).toMatchSnapshot()
     })
   })
 
   test(`Icon of unknown species matches snapshot`, () => {
-    expect(mount(<EbiSpeciesIcon species={`crocubot`} />)).toMatchSnapshot()
+    const tree = renderer
+      .create(<EbiSpeciesIcon species={`crocubot`} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
   })
 
   test(`uses EBI VF class names`, () => {
-    expect(shallow(<EbiSpeciesIcon species={`crocubot`} />)).toContainExactlyOneMatchingElement(`.icon.icon-species`)
+    render(<EbiSpeciesIcon species={`crocubot`} />)
+    const ebiSpeciesIcon = screen.getByTitle(strToRegExp(`crocubot`))
+    expect(ebiSpeciesIcon).toHaveClass(`icon icon-species`)
   })
 
   test(`Warm-blooded species are red, violets are blue`, () => {
-    const mappedMammals = allSpecies.filter((species) => lookUpIcon(species)[0] === `mammals`)
+    const mappedMammals = allSpecies.filter(species => lookUpIcon(species)[0] === `mammals`)
 
-    mappedMammals.forEach((warmBloodedAnimal) => {
-      expect(shallow(<EbiSpeciesIcon species={warmBloodedAnimal} />)).toHaveStyle(`color`, `red`)
+    mappedMammals.forEach(warmBloodedAnimal => {
+      render(<EbiSpeciesIcon species={warmBloodedAnimal} />)
+      const ebiSpeciesIcon = screen.getByTitle(strToRegExp(warmBloodedAnimal))
+      expect(ebiSpeciesIcon).toHaveStyle({ color: `red` })
     })
 
-    mappedMammals.forEach((warmBloodedAnimal) => {
-      expect(shallow(<EbiSpeciesIcon species={warmBloodedAnimal} groupColors={altGroupColors} />)).toHaveStyle(`color`, `brown`)
+    cleanup()
+
+    mappedMammals.forEach(warmBloodedAnimal => {
+      render(<EbiSpeciesIcon species={warmBloodedAnimal} groupColors={altGroupColors} />)
+      const ebiSpeciesIcon = screen.getByTitle(strToRegExp(warmBloodedAnimal))
+      expect(ebiSpeciesIcon).toHaveStyle({color: altGroupColors.mammals})
     })
   })
 
   test(`Plants are green`, () => {
-    const mappedPlants = allSpecies.filter((species) => lookUpIcon(species)[0] === `plants`)
+    const mappedPlants = allSpecies.filter(species => lookUpIcon(species)[0] === `plants`)
 
     mappedPlants.forEach(plant => {
+      render(<EbiSpeciesIcon species={plant} />)
+      const ebiSpeciesIcon = screen.getByTitle(strToRegExp(plant))
       // We’re happy if the colour is green-like
-      expect(shallow(<EbiSpeciesIcon species={plant} />).prop(`style`).color).toMatch(`green`)
+      expect(ebiSpeciesIcon.style.getPropertyValue(`color`)).toMatch(`green`)
     })
 
+    cleanup()
+
     mappedPlants.forEach(plant => {
-      expect(shallow(<EbiSpeciesIcon species={plant} groupColors={altGroupColors} />)).toHaveStyle(`color`, `orange`)
+      render(<EbiSpeciesIcon species={plant} groupColors={altGroupColors} />)
+      const ebiSpeciesIcon = screen.getByTitle(strToRegExp(plant))
+      expect(ebiSpeciesIcon).toHaveStyle({ color: altGroupColors.plants })
     })
   })
 
   test(`Roses are red, others are blue`, () => {
-    const mappedOthers = allSpecies.filter((species) => lookUpIcon(species)[0] === `other`)
+    const mappedOthers = allSpecies.filter(species => lookUpIcon(species)[0] === `other`)
 
     mappedOthers.forEach(other => {
+      render(<EbiSpeciesIcon species={other} />)
+      const ebiSpeciesIcon = screen.getByTitle(strToRegExp(other))
       // We’re happy if the colour is blue-like
-      expect(shallow(<EbiSpeciesIcon species={other} />).prop(`style`).color).toMatch(`blue`)
+      expect(ebiSpeciesIcon.style.getPropertyValue(`color`)).toMatch(`blue`)
     })
+
+    cleanup()
 
     mappedOthers.forEach(other => {
-      expect(shallow(<EbiSpeciesIcon species={other} groupColors={altGroupColors} />)).toHaveStyle(`color`, `yellow`)
+      render(<EbiSpeciesIcon species={other} groupColors={altGroupColors} />)
+      const ebiSpeciesIcon = screen.getByTitle(strToRegExp(other))
+      expect(ebiSpeciesIcon).toHaveStyle({ color: altGroupColors.other })
     })
   })
 
-  test(`admits a colour prop that overrides group colouring`, () => {
-    expect(shallow(<EbiSpeciesIcon species={`homo sapiens`} color={`yellow`}/>)).toHaveStyle(`color`, `yellow`)
-    expect(shallow(<EbiSpeciesIcon species={`homo sapiens`} groupColors={altGroupColors} color={`yellow`}/>)).toHaveStyle(`color`, `yellow`)
+  test(`admits a colour prop`, () => {
+    const species = `homo sapiens`
+    const customColour = `yellow`
+    render(<EbiSpeciesIcon species={species} color={customColour} />)
+    const ebiSpeciesIcon = screen.getByTitle(strToRegExp(species))
+    expect(ebiSpeciesIcon).toHaveStyle({ color: customColour })
   })
 
+  test(`admits a colour prop that has precedence over custom group colouring`, () => {
+    const species = `homo sapiens`
+    const customColour = `yellow`
+    render(<EbiSpeciesIcon species={species} color={customColour} groupColors={altGroupColors} />)
+    const ebiSpeciesIcon = screen.getByTitle(strToRegExp(species))
+    expect(ebiSpeciesIcon).toHaveStyle({ color: customColour })
+  })
 })
