0e5627e1370187f23a4a1b6081ad56b0e3335e5e
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-09-24 14:50:20 +0100

    Have some easy to switch demo pages

--- packages/scxa-cell-type-wheel-experiment-heatmap/html/Demo.js
+++ packages/scxa-cell-type-wheel-experiment-heatmap/html/Demo.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom'
 
 import CellTypeWheelExperimentHeatmap from '../src/index.js'
 
-import tcell from './data/tcell-sunburst.json'
-import heatmapData from './data/heatmap-data.json'
+import cellTypeWheelData from './data/leukocyte-sunburst.json'
+import heatmapData from './data/homo-sapiens-blood-t-cell-heatmap.json'
+const cellTypeWheelTitle = `leukocyte`
+const heatmapTitle = `T cell`
+
+//import cellTypeWheelData from './data/lung-sunburst.json'
+//import heatmapData from './data/cd.json'
+//const cellTypeWheelTitle = `lung`
+//const heatmapTitle = `CD4-positive, alpha-beta T cell`
 
 const render = (options, target) => {
   ReactDOM.render(
     <CellTypeWheelExperimentHeatmap
       {...options}
-      cellTypeWheelData={tcell}
+      cellTypeWheelSearchTerm={cellTypeWheelTitle}
+      cellTypeWheelData={cellTypeWheelData}
+      cellTypeHeatmapSearchTerm={heatmapTitle}
       cellTypeHeatmapData={heatmapData}
     />, document.getElementById(target)
   )
