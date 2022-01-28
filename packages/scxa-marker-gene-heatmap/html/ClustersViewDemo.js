930f646eea66ac1499f3384786004f7060378d27
lingyun1010 <zhaolingyun1010@gmail.com>
2022-01-28 11:27:43 +0000

    Add props and states for inferred cell type plot

--- packages/scxa-marker-gene-heatmap/html/ClustersViewDemo.js
+++ packages/scxa-marker-gene-heatmap/html/ClustersViewDemo.js
@@ -27,12 +27,30 @@ class Demo extends React.Component {
       // experimentAccession: `E-ENAD-19`,
       // experimentAccession: `E-GEOD-93593`,
       ks: ks,
-      ksWithMarkers: ['14','18','20'],
+      //https://www.ebi.ac.uk/gxa/sc/json/experiments/E-MTAB-5061/metadata/tsneplot
+      ksWithMarkers: ['9','14','18','20','24'],
       // ksWithMarkers: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
       // ksWithMarkers: [4, 7, 9],
       // ksWithMarkers: [36, 37, 38, 39, 40, 41, 42, 42, 43, 44, 45, 46],
-      selectedK: ks[2]
-      // selectedK: 17
+      selectedColourBy: 'inferred_cell_type_-_ontology_labels',
+      selectedColourByCategory: `metadata`,
+      metadata: [
+        {value:	"inferred_cell_type_-_ontology_labels",
+        label:	"Inferred cell type - ontology labels"
+        },
+        {
+          value:    "inferred_cell_type_-_authors_labels",
+          label:    "Inferred cell type - authors labels"
+        },
+        {
+          value:    "individual",
+          label:    "Individual"
+        },
+        {value:	"disease",
+        label:	"Disease"}
+      ],
+      cellTypes: [`mast cell`],
+      selectedClusterId: [`mast cell`]
     }
   }
 
@@ -42,22 +60,33 @@ class Demo extends React.Component {
         <HeatmapView
           wrapperClassName={`row expanded`}
           resource={
-            URI(`json/experiments/${this.state.experimentAccession}/marker-genes/clusters`)
-              .search({k: this.state.selectedK})
-              .toString()
+            this.state.selectedColourByCategory == `clusters` ?
+                URI(`json/experiments/${this.state.experimentAccession}/marker-genes/clusters`)
+                  .search({k: this.state.selectedColourBy})
+                  .toString() :
+                URI(`json/experiments/${this.state.experimentAccession}/marker-genes-heatmap/cell-types`)
+                  .search({cellGroupType: this.state.selectedColourBy, cellType: this.state.selectedClusterId})
+                  .toString()
           }
           host={`http://localhost:8080/gxa/sc/`}
           ks={this.state.ks}
           ksWithMarkers={this.state.ksWithMarkers}
-          selectedK={this.state.selectedK}
-          onSelectK={
-            (k) => {
-              this.setState({
-                selectedK: parseInt(k)
-              })
-            }
+          selectedColourByCategory={this.state.selectedColourByCategory}
+          selectedColourBy={this.state.selectedColourBy}
+          selectedClusterId={this.state.selectedClusterId}
+          cellTypes={this.state.cellTypes}
+          onChangeColourBy={
+          (colourByCategory, colourByValue) => {
+            this.setState({
+              selectedColourBy : colourByValue,
+              selectedColourByCategory : colourByCategory
+
+            })
+            this._resetHighlightClusters()
           }
-          species={`Homo sapiens`}
+        }
+        metadata={this.state.metadata}
+        species={`Homo sapiens`}
         />
       </div>
     )
