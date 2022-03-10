b546ebac2d6b10f064b21c0fa5673baf71f51854
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-03-10 13:05:32 +0000

    Rewrite component to use Markdown to render the banner

--- packages/atlas-information-banner/src/AtlasInformationBanner.js
+++ packages/atlas-information-banner/src/AtlasInformationBanner.js
@@ -1,4 +1,17 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
+
+const motd =
+`
+**Help us improve Single Cell Expression Atlas**
+
+We are continuously developing the resource to deliver the best possible service for the community;
+Please take two minutes to fill out our user survey and help us make Expression Atlas even better.
+
+<a target={_blank} href="https://www.surveymonkey.co.uk/r/SCEAsurvey22" class="button">Take survey</a>
+`
 
 // We’re leveraging EBI Visual Framework (i.e. Foundation) to make the component functional. See this package’s
 // html/index.html for a working example with all the necessary boilerplate.
@@ -6,12 +19,7 @@ import React from 'react'
 const AtlasInformationBanner = () => {
   return (
     <div className={`callout`} data-closable={``}>
-      <b>Help us improve Single Cell Expression Atlas</b>
-      <p>
-        We are continuously developing the resource to deliver the best possible service for the community;
-        Please take two minutes to fill out our user survey and help us make Expression Atlas even better.
-      </p>
-      <a target={`_blank`} href="https://www.surveymonkey.co.uk/r/SCEAsurvey22" className="button">Take survey</a>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{motd}</ReactMarkdown>
       <button className={`close-button`} aria-label={`Dismiss information banner`} type={`button`} data-close={``}>
         <span aria-hidden={true}>&times;</span>
       </button>
