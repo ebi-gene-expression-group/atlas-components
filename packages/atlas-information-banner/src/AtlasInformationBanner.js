bf48ef3a1552756d9e324c00ce1b5d6d8078b5da
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-03-10 12:17:18 +0000

    Transform AtlasInformationBanner to a function component
    If we use Foundation we don’t need a state prop to control the visibility of the banner

--- packages/atlas-information-banner/src/AtlasInformationBanner.js
+++ packages/atlas-information-banner/src/AtlasInformationBanner.js
@@ -1,33 +1,22 @@
 import React from 'react'
 
-class AtlasInformationBanner extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      visibility: ``
-    }
-  }
-
-  render() {
-    return(
-      <div className={`row`} style={{display: this.state.visibility}}>
-        <div className={`columns small-11`}>
-          <p>
-            <b>Help us improve Expression Atlas!</b>
-          </p>
-          <p>
-						We are continuously developing the resource to deliver the best possible service for the community.
-						Please take two minutes to fill out our user survey and help us make Expression Atlas even better.
-          </p>
-          <a target={`_blank`} href={`https://www.surveymonkey.co.uk/r/EAsurvey22`} className={`button`}>Take survey</a>
-        </div>
-        <div className={`columns small-1`}>
-          <a href={`#`} onClick={ () => this.setState({visibility: `none`}) } style={{fontSize:`xx-large`, border: `none`}}>&times;</a>
-        </div>
-      </div>
-    )
-  }
+// We’re leveraging EBI Visual Framework (i.e. Foundation) to make the component functional. See this package’s
+// html/index.html for a working example with all the necessary boilerplate.
+// For vanilla HTML see version 1 of this component.
+const AtlasInformationBanner = () => {
+  return (
+    <div className={`callout`} data-closable={``}>
+      <b>Help us improve Single Cell Expression Atlas</b>
+      <p>
+        We are continuously developing the resource to deliver the best possible service for the community;
+        Please take two minutes to fill out our user survey and help us make Expression Atlas even better.
+      </p>
+      <a target={`_blank`} href="https://www.surveymonkey.co.uk/r/SCEAsurvey22" className="button">Take survey</a>
+      <button className={`close-button`} aria-label={`Dismiss information banner`} type={`button`} data-close={``}>
+        <span aria-hidden={true}>&times;</span>
+      </button>
+    </div>
+  )
 }
 
 export default AtlasInformationBanner
