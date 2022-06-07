39eda7816eaf31acd302da8ab3ce79a6e6d98c52
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-06-07 10:53:24 +0100

    Move eslint-config out of Lerna’s control and update to latest eslint version

---
+++ eslint-config/index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+
+  plugins: [
+    'react'
+  ],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard'
+  ],
+
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+
+  rules: {
+    'no-var': 'error', // Enforce using const and let, rather than var
+    semi: ['warn', 'never'], // Disallow semicolons
+    quotes: ['warn', 'backtick'], // Enforce using `` for strings
+    eqeqeq: ['error', 'always'], // Enforce using === and !==
+    indent: ['error', 2], // Enforce indentation using 2 spaces
+    'no-console': ['warn', { allow: ['warn', 'error'] }] // Allow console.warn and console.error statements
+  },
+
+  overrides: [{
+    files: ['*.test.js'],
+    rules: {
+      'react/react-in-jsx-scope': 0
+    }
+  }]
+}
