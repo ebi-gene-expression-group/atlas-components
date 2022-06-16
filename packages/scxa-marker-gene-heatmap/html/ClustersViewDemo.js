6fb7350faf5664de17ef44756c5be7682dd1b3b9
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-06-16 10:46:25 +0100

    Revert "Feature #179733229 – Show cell types as well as clusters (#99)"
    This reverts commit f8dd89fd

--- packages/scxa-marker-gene-heatmap/html/ClustersViewDemo.js
+++ packages/scxa-marker-gene-heatmap/html/ClustersViewDemo.js
@@ -5,7 +5,7 @@ import URI from 'urijs'
 import HeatmapView from '../src/clustersView/ClustersHeatmapView'
 
 // K values for E-MTAB-5061
-const ks = ['8','11','19','21','25']
+const ks = [9, 14, 18, 20, 24, 34, 41, 44, 50]
 
 // K values for E-EHCA-2
 // const ks = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
@@ -27,33 +27,14 @@ class Demo extends React.Component {
       // experimentAccession: `E-ENAD-19`,
       // experimentAccession: `E-GEOD-93593`,
       ks: ks,
-      //https://www.ebi.ac.uk/gxa/sc/json/experiments/E-MTAB-5061/metadata/tsneplot
-      ksWithMarkers: ['8','11','19','21','25'],
+      ksWithMarkers: ['14','18','20'],
       // ksWithMarkers: [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
       // ksWithMarkers: [4, 7, 9],
       // ksWithMarkers: [36, 37, 38, 39, 40, 41, 42, 42, 43, 44, 45, 46],
-      selectedColourBy: 'inferred cell type - ontology labels',
-      selectedClusterCategory: `metadata`,
-      metadata: [
-        {
-          value:	"inferred cell type - ontology labels",
-          label:	"Inferred cell type - ontology labels"
-        },
-        {
-          value:    "inferred cell_type - authors labels",
-          label:    "Inferred cell type - authors labels"
-        },
-        {
-          value:    "individual",
-          label:    "Individual"
-        },
-        {value:	"disease",
-        label:	"Disease"}
-      ],
-      //endpoint: http://localhost:8080/gxa/sc//json/experiments/E-MTAB-5061/marker-genes-heatmap/cellTypeGroups?cellGroupType=inferred%20cell%20type%20-%20ontology%20labels
-      cellTypes: ["Not available","acinar cell","co-expression cell","endothelial cell","mast cell","pancreatic A cell","pancreatic D cell","pancreatic PP cell","pancreatic ductal cell","pancreatic endocrine cell","pancreatic epsilon cell","pancreatic stellate cell","professional antigen presenting cell","type B pancreatic cell"].sort(),
-      selectedClusterId: null
-  }}
+      selectedK: ks[2]
+      // selectedK: 17
+    }
+  }
 
   render() {
     return (
@@ -61,35 +42,21 @@ class Demo extends React.Component {
         <HeatmapView
           wrapperClassName={`row expanded`}
           resource={
-            this.state.selectedClusterCategory == `metadata` ?
-                URI(`json/experiments/${this.state.experimentAccession}/marker-genes-heatmap/cell-types`)
-                    //sort() is very important here! Otherwise, it will fetch though the values in list are not changed
-                    .search({cellGroupType: this.state.selectedColourBy, cellType: this.state.cellTypes.sort() })
-                    .toString() :
-                URI(`json/experiments/${this.state.experimentAccession}/marker-genes/clusters`)
-                  .search({k: this.state.selectedColourBy})
-                  .toString()
+            URI(`json/experiments/${this.state.experimentAccession}/marker-genes/clusters`)
+              .search({k: this.state.selectedK})
+              .toString()
           }
-          host={`https://wwwdev.ebi.ac.uk/gxa/sc/`}
+          host={`http://localhost:8080/gxa/sc/`}
           ks={this.state.ks}
           ksWithMarkers={this.state.ksWithMarkers}
-          selectedClusterCategory={this.state.selectedClusterCategory}
-          selectedClusterId={this.state.selectedColourBy}
-          selectedClusterId={this.state.selectedClusterId}
-          cellTypes={this.state.cellTypes}
-          onChangeClusterId={(colourByCategory, colourByValue) => {
-            this.setState({
-              selectedColourBy : colourByValue,
-              selectedColourByCategory : colourByCategory,
-              selectedClusterId: null
-            })
-          }}
-          onChangeMarkerGeneFor={(selectedOption) => {
-            this.setState((state) => ({
-              selectedClusterId: selectedOption
-            }))
-          }}
-          metadata={this.state.metadata}
+          selectedK={this.state.selectedK}
+          onSelectK={
+            (k) => {
+              this.setState({
+                selectedK: parseInt(k)
+              })
+            }
+          }
           species={`Homo sapiens`}
         />
       </div>
