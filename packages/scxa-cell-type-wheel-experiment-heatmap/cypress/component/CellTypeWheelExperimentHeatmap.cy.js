92125495115b0ee6a41179807da7f888620feb9e
Karoly Erdos <karoly@ebi.ac.uk>
2022-10-24 14:31:28 +0100

    Update the tests

--- packages/scxa-cell-type-wheel-experiment-heatmap/cypress/component/CellTypeWheelExperimentHeatmap.cy.js
+++ packages/scxa-cell-type-wheel-experiment-heatmap/cypress/component/CellTypeWheelExperimentHeatmap.cy.js
@@ -5,6 +5,10 @@ import speciesResponse from '../fixtures/speciesResponse.json'
 import cellTypeWheelData from '../fixtures/CellTypeDataForWheelResponse.json'
 import heatMapTCellResponse from '../fixtures/heatMapRegulatoryTCellResponse.json'
 
+function firstWordMatcherOf (str) {
+  return new RegExp(`.${str.split(` `)[0]}*`)
+}
+
 describe(`CellTypeWheelExperimentHeatmap`, () => {
   const props = {
     searchTerm: `t cell`
@@ -23,13 +27,18 @@ describe(`CellTypeWheelExperimentHeatmap`, () => {
     cy.contains(`Failed fetching species`)
   })
 
-  it(`mounts without data`, () => {
+  it(`mounts with data and check if the cell type wheel is displayed`, () => {
     cy.intercept(`GET`, `/gxa/sc/json/suggestions/species`,
       speciesResponse)
+    cy.intercept(`GET`, `/gxa/sc/json/cell-type-marker-genes/regulatory%20T%20cell?experiment-accessions=E-ENAD-15`,
+      heatMapTCellResponse)
     cy.viewport(800, 1000)
     cy.mount(<CellTypeWheelExperimentHeatmap
       {...props}
     />)
+    cy.get(`.highcharts-title`)
+      .invoke(`text`)
+      .then(text => assert.isTrue(firstWordMatcherOf(props.searchTerm).test(text)))
   })
 
   it(`mounts with data and displays a heatmap when a cell type label is clicked`, () => {
@@ -41,6 +50,7 @@ describe(`CellTypeWheelExperimentHeatmap`, () => {
     cy.mount(<CellTypeWheelExperimentHeatmap
       {...props}
     />)
+
     const ringElement = cy.contains(`regulatory T cell`)
     ringElement.click({ force: true })
     heatMapTCellResponse.map((cell) => cell.geneName).forEach(
