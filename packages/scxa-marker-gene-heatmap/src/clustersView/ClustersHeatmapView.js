95007ed1a7157a3b6175e746f952a0e57c3bf8da
Lingyun Zhao <33519183+lingyun1010@users.noreply.github.com>
2022-12-16 18:04:54 +0800

    📦 scxa-marker-gene-heatmap 💬 Bugfix #181594058 – Fix cluster label not showing on the right (#103)
    * Fix the xAxis and filtered data for heatmap, add a
    
    condition for plotting
    
    * Make filteredData prop optional, replaced by data
    
    if not provided
    
    * Refactor filteredData conditions
    
    * Refactor marker gene heatmap plotline object logic
    
    * Fix the label showing problem to adjust multiexp..
    
    celltype heatmap
    
    * Fix eslint errors
    
    * Refactor codes

--- packages/scxa-marker-gene-heatmap/src/clustersView/ClustersHeatmapView.js
+++ packages/scxa-marker-gene-heatmap/src/clustersView/ClustersHeatmapView.js
@@ -141,7 +141,7 @@ class HeatmapView extends React.Component {
           <div className={wrapperClassName}>
             <div className={plotWrapperClassName} style={{position: `relative`}}>
               <MarkerGeneHeatmap
-                data={filteredData}
+                data={selectedClusterId && selectedClusterId.value !== `all` ? filteredData : data}
                 isDataFiltered={selectedClusterId && selectedClusterId.value !== `all` || false}
                 xAxisCategories={allClusterIds}
                 yAxisCategories={_.chain(data).map(cell => _.pick(cell, `geneName`, `cellGroupValueWhereMarker`)).uniqWith(_.isEqual).map(`geneName`).value()}
