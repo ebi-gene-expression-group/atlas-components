3ea469238aabcc8a186b128999b02429bf316f03
Karoly Erdos <karoly@ebi.ac.uk>
2023-03-29 16:57:44 +0100

    Add FilterList component without fetch loader but processing new response structure from backend

---
+++ packages/scxa-faceted-search-results-v5/src/FilterList.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+class FilterList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      ascending: false,
+      sortTitle: props.sortTitle
+    }
+
+    this.sortEntries = this.sortEntries.bind(this)
+  }
+  sortEntries(attribute){
+    this.setState({
+      sortTitle: attribute,
+      ascending: !this.state.ascending
+    })
+  }
+
+  render() {
+    const {filteredResults, resultsMessage, ResultsHeaderClass, ResultElementClass} = this.props
+
+    const filteredElements = _.sortBy(filteredResults, this.state.sortTitle)
+    const sortedElements = this.state.ascending ? filteredElements :  filteredElements.reverse()
+
+    return (
+      <div>
+        <h4>{resultsMessage}</h4>
+        { sortedElements.length && <ResultsHeaderClass onClick={this.sortEntries}/> }
+        { sortedElements.map((element, index) => <div key={index}><ResultElementClass {...element}/></div>) }
+      </div>
+    )
+  }
+}
+
+FilterList.propTypes = {
+  filteredResults: PropTypes.arrayOf(PropTypes.object).isRequired,
+  resultsMessage: PropTypes.string,
+  ResultsHeaderClass: PropTypes.func,
+  ResultElementClass: PropTypes.func.isRequired,
+  sortTitle: PropTypes.string
+}
+
+FilterList.defaultProps = {
+  resultsMessage: ``
+}
+
+export default FilterList
