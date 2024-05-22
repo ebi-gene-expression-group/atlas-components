a7033c464a2e22a22580fe4dd8e40aeecf662b7c
lingyun zhao <zhaolingyun1010@gmail.com>
2024-05-22 11:24:03 +0100

    Export all styled css to an external file

--- packages/scxa-faceted-search-results/html/fetch-loader/ExperimentCard.js
+++ packages/scxa-faceted-search-results/html/fetch-loader/ExperimentCard.js
@@ -1,72 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 import formatNumber from 'format-number'
 import EbiSpeciesIcon from '@ebi-gene-expression-group/react-ebi-species'
+import {ExperimentIconDiv, CardContainerDiv, SmallIconDiv, IconDiv, MarkerDiv, TitleDiv, VariableDiv, CountDiv}from './ExperimentCardDiv'
 
 const _formatNumber = formatNumber()
-
-const ExperimentIconDiv = styled.div`
-  background-color: ${props => props.background};
-  color: ${props => props.color};
-  border-radius: 50%;
-  font-size: 16px;
-  height: 20px;
-  width: 20px;
-  text-align: center;
-  vertical-align: middle;
-  margin-left: 4px;
-  display: inline-block;
-`
-
-const CardContainerDiv = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex !important;
-  flex-wrap: nowrap;
-  align-items: center;
-  border: #e6e6e6 solid 1px;
-  margin-bottom: 0.5rem;
-  padding: 1rem;
-  &:hover {
-    background-color: #eaeaea;
-    cursor: pointer;
-  }
-`
-
-const SmallIconDiv = styled.div`
-  width: 5%;
-  text-align: center;
-  font-size: 1rem;
-  display: inline-flex;
-`
-
-const IconDiv = styled.div`
-  width: 15%;
-  text-align: center;
-  font-size: 3rem;
-`
-
-const MarkerDiv = styled.div`
-  width: 15%;
-  text-align: center;
-`
-
-const TitleDiv = styled.p`
-  width: 40%;
-  text-align: center;
-  margin-bottom: 0;
-`
-
-const VariableDiv = styled.div`
-  width: 20%;
-  text-align: center;
-`
-
-const CountDiv = styled.div`
-  width: 10%;
-  text-align: center;
-`
 
 const ANNDATA = `E-ANND`
 
@@ -89,7 +27,6 @@ class ExperimentCard extends React.Component {
       []
 
     const showIcon = experimentAccession.startsWith(ANNDATA)
-      console.log(experimentAccession, showIcon)
 
     return (
       <CardContainerDiv onClick={this._goToExperiment.bind(this, url)}>
