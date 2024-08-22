633a25dd47e1c40ae3aa4204954c46c1ed530203
lingyun zhao <zhaolingyun1010@gmail.com>
2024-08-22 17:05:25 +0100

    Refactor html demo with json data

--- packages/scxa-marker-gene-heatmap/html/markerGeneHeatmapRenderModule.js
+++ packages/scxa-marker-gene-heatmap/html/markerGeneHeatmapRenderModule.js
@@ -4,20 +4,28 @@ import ReactDOM from 'react-dom'
 import _ from 'lodash'
 
 import MarkerGeneHeatmap from "../src/MarkerGeneHeatmap"
+import cellTypeData from "./cell-types-data.json"
+import clusterData from "./clusters-data.json"
+import multiexperimentcelltypesData from "./multiexperiment-cell-types-data.json"
 
+const dataList = {
+  cellTypes: cellTypeData,
+  clusters: clusterData,
+  multiexperimentcelltypes: multiexperimentcelltypesData
+}
 const render = ({ data, species, heatmapType, defaultHeatmapHeight, hasDynamicHeight, heatmapRowHeight }, target) => {
-  ReactDOM.render(
+  return (ReactDOM.render(
     <MarkerGeneHeatmap
-      data={data}
-      xAxisCategories={_.chain(data).uniqBy(`x`).sortBy(`x`).map(`cellGroupValue`).value()}
-      yAxisCategories={_.chain(data).uniqBy(`y`).sortBy(`y`).map(`geneName`).value()}
+      data={dataList[data]}
+      xAxisCategories={_.chain(dataList[data]).uniqBy(`x`).sortBy(`x`).map(`cellGroupValue`).value()}
+      yAxisCategories={_.chain(dataList[data]).uniqBy(`y`).sortBy(`y`).map(`geneName`).value()}
       chartHeight={defaultHeatmapHeight}
-      hasDynamicHeight={_.chain(data).map(`geneName`).uniq().value().length > 5 ? hasDynamicHeight : false}
+      hasDynamicHeight={_.chain(dataList[data]).map(`geneName`).uniq().value().length > 5 ? hasDynamicHeight : false}
       heatmapRowHeight={heatmapRowHeight}
       species={species}
       heatmapType={heatmapType}
       cellType={`Very Long Cell Type Title`}
-    />, document.getElementById(target))
+    />, document.getElementById(target)))
 }
 
 export { render }
