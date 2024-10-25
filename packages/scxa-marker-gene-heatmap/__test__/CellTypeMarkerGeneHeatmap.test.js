48f9b6d68fa33c89573b4189ebbce3065168ff9f
lingyun zhao <zhaolingyun1010@gmail.com>
2024-10-25 11:19:38 +0100

    Refactor test descriptions and props values

--- packages/scxa-marker-gene-heatmap/__test__/CellTypeMarkerGeneHeatmap.test.js
+++ packages/scxa-marker-gene-heatmap/__test__/CellTypeMarkerGeneHeatmap.test.js
@@ -1,37 +1,39 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import MarkerGeneHeatmap from '../src/MarkerGeneHeatmap'
 
 describe(`MarkerGeneHeatmap`, () => {
   test(`creates plot lines for every cell type`, () => {
+    const data = [
+      {
+        x: 0,
+        y: 0,
+        geneName: `foo`,
+        value: 13,
+        cellGroupValue: `1`,
+        cellGroupValueWhereMarker: `1`
+      },
+      {
+        x: 1,
+        y: 1,
+        geneName: `bar`,
+        value: 2,
+        cellGroupValue: `2`,
+        cellGroupValueWhereMarker: `2`
+      },
+      {
+        x: 2,
+        y: 2,
+        geneName: `foobar`,
+        value: 1,
+        cellGroupValue: `3`,
+        cellGroupValueWhereMarker: `3`
+      }
+    ]
     const { container } = render(
       <MarkerGeneHeatmap
-        data={[
-          {
-            x: 0,
-            y: 0,
-            geneName: `foo`,
-            value: 13,
-            cellGroupValue: `1`,
-            cellGroupValueWhereMarker: `1`
-          },
-          {
-            x: 1,
-            y: 1,
-            geneName: `bar`,
-            value: 2,
-            cellGroupValue: `2`,
-            cellGroupValueWhereMarker: `2`
-          },
-          {
-            x: 2,
-            y: 2,
-            geneName: `foobar`,
-            value: 1,
-            cellGroupValue: `3`,
-            cellGroupValueWhereMarker: `3`
-          }
-        ]}
+        data={data}
         xAxisCategories={[`1`, `2`, `3`]}
         yAxisCategories={[`a`, `b`, `c`]}
         chartHeight={200}
@@ -43,10 +45,10 @@ describe(`MarkerGeneHeatmap`, () => {
     )
 
     const plotLines = container.querySelectorAll(`[class^="highcharts-plot-line "]`)
-    expect(plotLines).toHaveLength(3)
+    expect(plotLines).toHaveLength(data.length)
   })
 
-  test(`has data export options and a styled button`, () => {
+  test(`has a download button`, () => {
     const { container } = render(
       <MarkerGeneHeatmap
         data={[
@@ -56,7 +58,8 @@ describe(`MarkerGeneHeatmap`, () => {
             geneName: `foo`,
             value: 13,
             cellGroupValue: `1`,
-            cellGroupValueWhereMarker: `1`
+            cellGroupValueWhereMarker: `1`,
+            expressionUnit: `butter`
           }
         ]}
         xAxisCategories={[`1`, `2`, `3`]}
@@ -68,8 +71,9 @@ describe(`MarkerGeneHeatmap`, () => {
         heatmapType="celltypes"
       />
     )
-
     // Use screen to query the button
-    expect(screen.getByText(`Download`)).toBeInTheDocument()
+    const button = screen.getByText(`Download`)
+    userEvent.click(button)
+    expect(button).toBeInTheDocument()
   })
 })
