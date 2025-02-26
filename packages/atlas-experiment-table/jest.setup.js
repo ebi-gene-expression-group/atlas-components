3085189308e600254758fcfaec989bde34be4937
lingyun zhao <zhaolingyun1010@gmail.com>
2025-02-26 16:42:35 +0000

    Update jest test enrironment and config files

--- packages/atlas-experiment-table/jest.setup.js
+++ packages/atlas-experiment-table/jest.setup.js
@@ -1,5 +1,5 @@
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
 
-// Configure Enzyme
-configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() });
