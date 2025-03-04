2ccaa22fc0132a4416cadc1c4bfa6ae2124373b4
lingyun zhao <zhaolingyun1010@gmail.com>
2025-03-04 16:55:31 +0000

    Refactor handler to msw version 2

--- packages/scxa-gene-search-form/__test__/fetch-mocks/handlers.js
+++ packages/scxa-gene-search-form/__test__/fetch-mocks/handlers.js
@@ -2,42 +2,50 @@ import { http, HttpResponse } from 'msw'
 
 export const handlers = [
   http.get('/gxa/sc/suggest', async ({ request }) => {
-    return HttpResponse.json([
-      {
-        label: 'Some category',
-        options: [
+    console.log('✅ MSW Handler triggered for /gxa/sc/suggest')
+
+    return new HttpResponse(
+        JSON.stringify([
           {
-            label: 'Suggestion 1',
-            value: JSON.stringify({
-              term: 'Suggestion 1',
-              category: 'some category',
-            }),
+            label: 'Some category',
+            options: [
+              {
+                label: 'Suggestion 1',
+                value: JSON.stringify({
+                  term: 'Suggestion 1',
+                  category: 'some category',
+                }),
+              },
+              {
+                label: 'Suggestion 2',
+                value: JSON.stringify({
+                  term: 'Suggestion 2',
+                  category: 'some category',
+                }),
+              },
+            ],
           },
           {
-            label: 'Suggestion 2',
-            value: JSON.stringify({
-              term: 'Suggestion 2',
-              category: 'some category',
-            }),
+            label: 'Another category',
+            options: [
+              {
+                label: 'Suggestion 3',
+                value: JSON.stringify({
+                  term: 'Suggestion 3',
+                  category: 'another category',
+                }),
+              },
+            ],
           },
-        ],
-      },
-      {
-        label: 'Another category',
-        options: [
-          {
-            label: 'Suggestion 3',
-            value: JSON.stringify({
-              term: 'Suggestion 3',
-              category: 'another category',
-            }),
-          },
-        ],
-      },
-    ])
+        ]),
+        { status: 200, headers: { 'Content-Type': 'application/json' } }
+    )
   }),
 
   http.get('/gxa/sc/search', async ({ request }) => {
-    return HttpResponse.json({}, { status: 200 })
+    return new HttpResponse(
+        JSON.stringify({}),
+        { status: 200, headers: { 'Content-Type': 'application/json' } }
+    )
   }),
 ]
