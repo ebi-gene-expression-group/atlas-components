e7b23617b4ca718aa9910197d98680c4956458ed
lingyun zhao <zhaolingyun1010@gmail.com>
2025-03-11 16:28:04 +0000

    Fix eslint errors

--- packages/scxa-gene-search-form/html/render.js
+++ packages/scxa-gene-search-form/html/render.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 
 import LabelledSelect from '../src/LabelledSelect'
 
-import { render as geneSearchFromRender } from '../src/index';
+import { render as geneSearchFromRender } from '../src/index'
 
 const render = (options, target) => {
-  geneSearchFromRender({...options, host: `${process.env.SERVICE_URL}/gxa/sc/`} , target);
-};
+  geneSearchFromRender({...options, host: `${process.env.SERVICE_URL}/gxa/sc/`} , target)
+}
 
 export { render }
 
