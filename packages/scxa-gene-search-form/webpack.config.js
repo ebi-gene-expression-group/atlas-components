b9dc89b435cdfbc1b59d0df592b41625c34c8000
lingyun zhao <zhaolingyun1010@gmail.com>
2025-03-11 16:24:12 +0000

    Update webpack config with env host plugin

--- packages/scxa-gene-search-form/webpack.config.js
+++ packages/scxa-gene-search-form/webpack.config.js
@@ -1,48 +1,58 @@
+const webpack = require('webpack')
+
 const vendorsBundleName = `vendors`
 
-module.exports = {
-  entry: {
-    geneSearchFormDemo: `./html/render.js`
-  },
-
-  output: {
-    library: `[name]`,
-    filename: `[name].bundle.js`,
-    clean: true
-  },
-
-  optimization: {
-    runtimeChunk: {
-       name: vendorsBundleName
+module.exports =  (env) => {
+  return {
+    entry: {
+      geneSearchFormDemo: `./html/render.js`
     },
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: vendorsBundleName,
-          chunks: 'all'
+
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.SERVICE_URL': JSON.stringify(env.host || 'http://localhost:8080/')
+      })
+    ],
+
+    output: {
+      library: `[name]`,
+      filename: `[name].bundle.js`,
+      clean: true
+    },
+
+    optimization: {
+      runtimeChunk: {
+        name: vendorsBundleName
+      },
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: vendorsBundleName,
+            chunks: 'all'
+          }
         }
       }
-    }
-  },
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/i,
-        exclude: /node_modules\//,
-        use: `babel-loader`
+    module: {
+      rules: [
+        {
+          test: /\.js$/i,
+          exclude: /node_modules\//,
+          use: `babel-loader`
+        }
+      ]
+    },
+
+    devServer: {
+      port: 9000,
+      static: `./html`,
+      devMiddleware: {
+        publicPath: `/dist`
       }
-    ]
-  },
-
-  devServer: {
-    port: 9000,
-    static: `./html`,
-    devMiddleware: {
-      publicPath: `/dist`
+      // Add if developing a SPA to redirect non-matching routes known by WDS (i.e. no document in /html) to the router
+      // historyApiFallback: true
     }
-    // Add if developing a SPA to redirect non-matching routes known by WDS (i.e. no document in /html) to the router
-    // historyApiFallback: true
   }
 }
