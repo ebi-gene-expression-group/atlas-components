ae18169a414ecc9ef33336b333ec2bfdbe446761
lingyun zhao <zhaolingyun1010@gmail.com>
2025-03-11 16:24:59 +0000

    Add host from plugin env to render component

--- packages/scxa-gene-search-form/html/render.js
+++ packages/scxa-gene-search-form/html/render.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom'
 
 import LabelledSelect from '../src/LabelledSelect'
 
+import { render as geneSearchFromRender } from '../src/index';
+
+const render = (options, target) => {
+  geneSearchFromRender({...options, host: `${process.env.SERVICE_URL}/gxa/sc/`} , target);
+};
+
+export { render }
+
 const renderLabelledSelect = (options, target) => {
   ReactDOM.render(<LabelledSelect {...options} />, document.getElementById(target))
 }
 
-export { render } from '../src/index'
 export { renderLabelledSelect }
