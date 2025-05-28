b6c099fba8177f43875837ffd822aa0164ddd90a
lingyun zhao <zhaolingyun1010@gmail.com>
2025-05-28 10:56:45 +0100

    Add section name as a header with props

--- packages/scxa-experiment-page/src/supplementary-information/AnalysisMethodsTable.js
+++ packages/scxa-experiment-page/src/supplementary-information/AnalysisMethodsTable.js
@@ -3,30 +3,34 @@ import PropTypes from 'prop-types'
 
 import Linkify from 'linkifyjs/react'
 
-const AnalysisMethodsTable = ({ data }) =>
-  <table>
-    <thead>
-      <tr>
-        {data[0].map((element, index) => <th key={index}>{element}</th>)}
-      </tr>
-    </thead>
-    <tbody>
-      {
-        data.slice(1, data.length).map((row, index) =>
-          <tr key={index}>
-            {row.map((element, index) =>
-              <td key={index}>
-                <Linkify>{element}</Linkify>
-              </td>
-            )}
-          </tr>
-        )
-      }
-    </tbody>
-  </table>
+const AnalysisMethodsTable = ({data, sectionName}) =>
+  <div>
+    <h3>{sectionName}</h3>
+    <table>
+      <thead>
+        <tr>
+          {data[0].map((element, index) => <th key={index}>{element}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.slice(1, data.length).map((row, index) =>
+            <tr key={index}>
+              {row.map((element, index) =>
+                <td key={index}>
+                  <Linkify>{element}</Linkify>
+                </td>
+              )}
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  </div>
 
 AnalysisMethodsTable.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired
+  data: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
+  sectionName: PropTypes.string.isRequired
 }
 
 export default AnalysisMethodsTable
